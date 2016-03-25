0edd03875b973b8b542ba0cae0a01385e4efcd34
Kirill (Home) <kirill.shlenskiy@gmail.com>
2016-03-25 13:48:09 +1100

    Percentage tweaks

--- src/Kirkin/Percentage.cs
+++ src/Kirkin/Percentage.cs
@@ -10,24 +10,10 @@ namespace Kirkin
         /// <summary>
         /// Calculates the percentage based on the given position and total figures.
         /// </summary>
-        public static int Calculate(int current, int max)
-        {
-            if (max <= 0) throw new ArgumentOutOfRangeException("max");
-            if (current < 0) throw new ArgumentOutOfRangeException("current");
-            if (current > max) throw new ArgumentException("Current cannot be greater than Max.");
-
-            int result = (int)((float)current / max * 100f);
-
-            return Math.Min(result, 100);
-        }
-
-        /// <summary>
-        /// Calculates the percentage based on the given position and total figures.
-        /// </summary>
         public static int Calculate(long current, long max)
         {
-            if (max <= 0) throw new ArgumentOutOfRangeException("max");
-            if (current < 0) throw new ArgumentOutOfRangeException("current");
+            if (max <= 0) throw new ArgumentOutOfRangeException(nameof(max));
+            if (current < 0) throw new ArgumentOutOfRangeException(nameof(current));
             if (current > max) throw new ArgumentException("Current cannot be greater than Max.");
 
             int result = (int)((double)current / max * 100.0);
