da53538aead2b60c47eeb072b45b0d0261448e74
Kirill (Home) <kirill.shlenskiy@gmail.com>
2016-03-26 10:22:41 +1100

    ExpressionEngine: caching provisions

--- src/Kirkin.Experimental/src/Kirkin/Linq/Expressions/ExpressionEngine.cs
+++ src/Kirkin.Experimental/src/Kirkin/Linq/Expressions/ExpressionEngine.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿//#define CACHING
+
+using System;
+using System.Collections.Concurrent;
 using System.Linq.Expressions;
 using System.Reflection;
+
+using Kirkin.Reflection;
 
 namespace Kirkin.Linq.Expressions
 {
@@ -9,6 +14,12 @@ namespace Kirkin.Linq.Expressions
         static class Cache<T>
         {
             internal static readonly ParameterExpression Param = Expression.Parameter(typeof(T), "o");
+
+            internal static readonly ConcurrentDictionary<MemberInfo, Expression> Getters
+                = new ConcurrentDictionary<MemberInfo, Expression>(MemberInfoEqualityComparer.Instance);
+
+            internal static readonly ConcurrentDictionary<MemberInfo, Expression> Setters
+                = new ConcurrentDictionary<MemberInfo, Expression>(MemberInfoEqualityComparer.Instance);
         }
 
         /// <summary>
@@ -18,13 +29,23 @@ namespace Kirkin.Linq.Expressions
         {
             if (fieldInfo == null) throw new ArgumentNullException(nameof(fieldInfo));
 
-            ParameterExpression param = Cache<TObject>.Param;
+            Expression expression;
+#if CACHING
+            if (!Cache<TObject>.Getters.TryGetValue(fieldInfo, out expression))
+            {
+#endif
+                ParameterExpression param = Cache<TObject>.Param;
 
-            // o => o.Field;
-            return Expression.Lambda<Func<TObject, TField>>(
-                Expression.Field(param, fieldInfo),
-                param
-            );
+                // o => o.Field;
+                expression = Expression.Lambda<Func<TObject, TField>>(
+                    Expression.Field(param, fieldInfo),
+                    param
+                );
+#if CACHING
+                Cache<TObject>.Getters.TryAdd(fieldInfo, expression);
+            }
+#endif
+            return (Expression<Func<TObject, TField>>)expression;
         }
 
         /// <summary>
@@ -34,15 +55,25 @@ namespace Kirkin.Linq.Expressions
         {
             if (fieldInfo == null) throw new ArgumentNullException(nameof(fieldInfo));
 
-            ParameterExpression param = Cache<TObject>.Param;
-            ParameterExpression value = Expression.Parameter(typeof(TField), "value");
+            Expression expression;
+#if CACHING
+            if (!Cache<TObject>.Setters.TryGetValue(fieldInfo, out expression))
+            {
+#endif
+                ParameterExpression param = Cache<TObject>.Param;
+                ParameterExpression value = Expression.Parameter(typeof(TField), "value");
 
-            // (o, value) => o.Field = value;
-            return Expression.Lambda<Action<TObject, TField>>(
-                Expression.Assign(Expression.Field(param, fieldInfo), value),
-                param,
-                value
-            );
+                // (o, value) => o.Field = value;
+                expression = Expression.Lambda<Action<TObject, TField>>(
+                    Expression.Assign(Expression.Field(param, fieldInfo), value),
+                    param,
+                    value
+                );
+#if CACHING
+                Cache<TObject>.Setters.TryAdd(fieldInfo, expression);
+            }
+#endif
+            return (Expression<Action<TObject, TField>>)expression;
         }
 
         /// <summary>
@@ -52,13 +83,23 @@ namespace Kirkin.Linq.Expressions
         {
             if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
 
-            ParameterExpression param = Cache<TObject>.Param;
+            Expression expression;
+#if CACHING
+            if (!Cache<TObject>.Getters.TryGetValue(propertyInfo, out expression))
+            {
+#endif
+                ParameterExpression param = Cache<TObject>.Param;
 
-            // o => o.Property;
-            return Expression.Lambda<Func<TObject, TProperty>>(
-                Expression.Property(param, propertyInfo),
-                param
-            );
+                // o => o.Property;
+                expression = Expression.Lambda<Func<TObject, TProperty>>(
+                    Expression.Property(param, propertyInfo),
+                    param
+                );
+#if CACHING
+                Cache<TObject>.Getters.TryAdd(propertyInfo, expression);
+            }
+#endif
+            return (Expression<Func<TObject, TProperty>>)expression;
         }
 
         /// <summary>
@@ -68,15 +109,25 @@ namespace Kirkin.Linq.Expressions
         {
             if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
 
-            ParameterExpression param = Cache<TObject>.Param;
-            ParameterExpression value = Expression.Parameter(typeof(TProperty), "value");
+            Expression expression;
+#if CACHING
+            if (!Cache<TObject>.Setters.TryGetValue(propertyInfo, out expression))
+            {
+#endif
+                ParameterExpression param = Cache<TObject>.Param;
+                ParameterExpression value = Expression.Parameter(typeof(TProperty), "value");
 
-            // (o, value) => o.Property = value;
-            return Expression.Lambda<Action<TObject, TProperty>>(
-                Expression.Assign(Expression.Property(param, propertyInfo), value),
-                param,
-                value
-            );
+                // (o, value) => o.Property = value;
+                expression = Expression.Lambda<Action<TObject, TProperty>>(
+                    Expression.Assign(Expression.Property(param, propertyInfo), value),
+                    param,
+                    value
+                );
+#if CACHING
+                Cache<TObject>.Setters.TryAdd(propertyInfo, expression);
+            }
+#endif
+            return (Expression<Action<TObject, TProperty>>)expression;
         }
     }
 }
