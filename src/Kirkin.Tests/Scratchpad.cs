3b530fb1fc5a048fae76a561203e196bc2db68d1
Kirill (Home) <kirill.shlenskiy@gmail.com>
2016-03-25 19:42:47 +1100

    Clean Scratchpad

--- src/Kirkin.Tests/Scratchpad.cs
+++ src/Kirkin.Tests/Scratchpad.cs
@@ -10,7 +10,6 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using Kirkin.Logging;
-using Kirkin.Reflection;
 
 using Xunit;
 using Xunit.Abstractions;
@@ -26,52 +25,6 @@ namespace Kirkin.Tests
             Output = Logger
                 .Create(output.WriteLine)
                 .WithFormatters(EntryFormatter.TimestampNonEmptyEntries());
-        }
-
-
-        [Fact]
-        public void GetRightFactoryWithActivator()
-        {
-            PropertyInfo property = typeof(string).GetProperty("Length");
-
-            for (int i = 0; i < 200000; i++)
-            {
-                IPropertyAccessorFactory factory = (IPropertyAccessorFactory)Activator.CreateInstance(
-                    typeof(PropertyAccessorFactory<,>).MakeGenericType(property.DeclaringType, property.PropertyType)
-                );
-
-                IPropertyAccessor accessor = factory.Create(property);
-            }
-        }
-
-        //[Fact]
-        //public void GetRightFactoryWithConstructorInfo()
-        //{
-        //    PropertyInfo property = typeof(string).GetProperty("Length");
-
-        //    for (int i = 0; i < 200000; i++)
-        //    {
-        //        IPropertyAccessor accessor = (IPropertyAccessor)typeof(PropertyAccessor<,>)
-        //            .MakeGenericType(property.DeclaringType, property.PropertyType)
-        //            .GetConstructor(new[] { typeof(PropertyInfo) })
-        //            .Invoke(new[] { property });
-
-        //        Assert.NotNull(accessor);
-        //    }
-        //}
-
-        interface IPropertyAccessorFactory
-        {
-            IPropertyAccessor Create(PropertyInfo property);
-        }
-
-        sealed class PropertyAccessorFactory<TTarget, TProperty>
-            : IPropertyAccessorFactory
-        {
-            public IPropertyAccessor Create(PropertyInfo property)
-            {
-                return new PropertyAccessor<TTarget, TProperty>(property);
-            }
         }
     }
 }
