f46d952bca0aac57aa87a93825fd0cb0762e1ca1
Kirill (Home) <kirill.shlenskiy@gmail.com>
2016-03-26 03:05:04 +1100

    Experimental: ExpressionEngine

---
+++ src/Kirkin.Experimental/src/Kirkin/Linq/Expressions/ExpressionEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kirkin.Linq.Expressions
+{
+    internal static class ExpressionEngine
+    {
+        public static Expression<Func<TObject, TField>> FieldGetter<TObject, TField>(FieldInfo fieldInfo)
+        {
+            if (fieldInfo == null) throw new ArgumentNullException(nameof(fieldInfo));
+
+            ParameterExpression param = Expression.Parameter(typeof(TObject), "o");
+
+            // o => o.Field;
+            return Expression.Lambda<Func<TObject, TField>>(
+                Expression.Field(param, fieldInfo),
+                param
+            );
+        }
+
+        public static Expression<Action<TObject, TField>> FieldSetter<TObject, TField>(FieldInfo fieldInfo)
+        {
+            if (fieldInfo == null) throw new ArgumentNullException(nameof(fieldInfo));
+
+            ParameterExpression param = Expression.Parameter(typeof(TObject), "o");
+            ParameterExpression value = Expression.Parameter(typeof(TField), "value");
+
+            // (o, value) => o.Field = value;
+            return Expression.Lambda<Action<TObject, TField>>(
+                Expression.Assign(Expression.Field(param, fieldInfo), value),
+                param,
+                value
+            );
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Linq/Expressions/ExpressionEngineTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Kirkin.Linq.Expressions;
+
+using Xunit;
+
+namespace Kirkin.Tests.Linq.Expressions
+{
+    public class ExpressionEngineTests
+    {
+        [Fact]
+        public void FieldGetter()
+        {
+            Dummy dummy = new Dummy { ID = 123 };
+            FieldInfo id = typeof(Dummy).GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
+            Expression<Func<Dummy, int>> getter = ExpressionEngine.FieldGetter<Dummy, int>(id);
+
+            Assert.Equal(123, getter.Compile().Invoke(dummy));
+        }
+
+        [Fact]
+        public void FieldSetter()
+        {
+            Dummy dummy = new Dummy { ID = 0 };
+            FieldInfo id = typeof(Dummy).GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
+            Expression<Action<Dummy, int>> setter = ExpressionEngine.FieldSetter<Dummy, int>(id);
+
+            setter.Compile().Invoke(dummy, 123);
+
+            Assert.Equal(123, dummy.ID);
+        }
+
+        sealed class Dummy
+        {
+            private int _id;
+
+            public int ID
+            {
+                get
+                {
+                    return _id;
+                }
+                set
+                {
+                    _id = value;
+                }
+            }
+        }
+    }
+}
