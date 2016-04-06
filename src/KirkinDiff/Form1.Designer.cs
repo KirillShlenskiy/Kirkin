f6322cd3c668596b0512e86b977123304a146f29
Kirill (Home) <kirill.shlenskiy@gmail.com>
2016-04-07 01:04:22 +1000

    KirkinDiff: work in progress

--- src/KirkinDiff/Form1.Designer.cs
+++ src/KirkinDiff/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConnectionStringTextBox1 = new System.Windows.Forms.TextBox();
+            this.CommandTextTextBox1 = new System.Windows.Forms.TextBox();
+            this.Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel.Controls.Add(this.splitContainer1);
+            this.Panel.Location = new System.Drawing.Point(12, 12);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(684, 499);
+            this.Panel.TabIndex = 0;
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ExecuteButton.Location = new System.Drawing.Point(12, 517);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(75, 23);
+            this.ExecuteButton.TabIndex = 1;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.CommandTextTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.ConnectionStringTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 499);
+            this.splitContainer1.SplitterDistance = 334;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String";
+            // 
+            // ConnectionStringTextBox1
+            // 
+            this.ConnectionStringTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConnectionStringTextBox1.Location = new System.Drawing.Point(6, 17);
+            this.ConnectionStringTextBox1.Name = "ConnectionStringTextBox1";
+            this.ConnectionStringTextBox1.Size = new System.Drawing.Size(325, 20);
+            this.ConnectionStringTextBox1.TabIndex = 1;
+            // 
+            // CommandTextTextBox1
+            // 
+            this.CommandTextTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CommandTextTextBox1.Location = new System.Drawing.Point(6, 44);
+            this.CommandTextTextBox1.Multiline = true;
+            this.CommandTextTextBox1.Name = "CommandTextTextBox1";
+            this.CommandTextTextBox1.Size = new System.Drawing.Size(325, 452);
+            this.CommandTextTextBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(708, 552);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.Panel);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "KirkinDiff";
+            this.Panel.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox ConnectionStringTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CommandTextTextBox1;
     }
 }
 
