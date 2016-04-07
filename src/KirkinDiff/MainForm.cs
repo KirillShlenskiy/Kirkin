8c251032ab3e96f9c122855309a41094b1130490
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-04-07 10:23:42 +1000

    KirkinDiff: timed execution

--- src/KirkinDiff/MainForm.cs
+++ src/KirkinDiff/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -39,33 +40,44 @@ namespace KirkinDiff
 
         private void ExecuteDiff()
         {
-            using (DataSet ds1 = ProduceDataSet(ConnectionStringTextBox1.Text, CommandTextTextBox1.Text))
-            using (DataSet ds2 = ProduceDataSet(ConnectionStringTextBox2.Text, CommandTextTextBox2.Text))
+            TimeSpan timeTaken1;
+            TimeSpan timeTaken2;
+
+            using (DataSet ds1 = ProduceDataSet(ConnectionStringTextBox1.Text, CommandTextTextBox1.Text, out timeTaken1))
+            using (DataSet ds2 = ProduceDataSet(ConnectionStringTextBox2.Text, CommandTextTextBox2.Text, out timeTaken2))
             {
                 DiffResult diff = DataSetDiff.Compare(ds1, ds2);
+                string timeTakenString = $"Time taken (left): {(double)timeTaken1.Milliseconds / 1000:0.###}s, right: {(double)timeTaken2.Milliseconds / 1000:0.###}s";
 
                 if (diff.AreSame)
                 {
                     MessageBox.Show(
+                        timeTakenString +
+                        Environment.NewLine +
                         $"Result sets identical. Tables: {ds1.Tables.Count}, Rows: {ds1.Tables.Cast<DataTable>().Sum(dt => dt.Rows.Count)}."
                     );
                 }
                 else
                 {
-                    MessageBox.Show(diff.ToString(DiffTextFormat.Indented));
+                    MessageBox.Show(
+                        timeTakenString + Environment.NewLine + diff.ToString(DiffTextFormat.Indented)
+                    );
                 }
             }
         }
 
-        private DataSet ProduceDataSet(string connectionString, string commandText)
+        private DataSet ProduceDataSet(string connectionString, string commandText, out TimeSpan timeTaken)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             using (SqlCommand command = new SqlCommand(commandText, connection))
             using (SqlDataAdapter adapter = new SqlDataAdapter(command))
             {
                 DataSet ds = new DataSet();
+                Stopwatch sw = Stopwatch.StartNew();
 
                 adapter.Fill(ds);
+
+                timeTaken = sw.Elapsed;
 
                 return ds;
             }
