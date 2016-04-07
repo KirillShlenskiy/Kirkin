8853ae3d85914eeff470c57214944d504c971ca1
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-04-07 11:17:13 +1000

    KirkinDiff: better syntax highlighting

--- src/KirkinDiff/MainForm.Designer.cs
+++ src/KirkinDiff/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.Panel = new System.Windows.Forms.Panel();
             this.SplitContainer = new System.Windows.Forms.SplitContainer();
@@ -38,11 +39,15 @@
             this.ConnectionStringTextBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.ExecuteButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.Panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
             this.SplitContainer.Panel1.SuspendLayout();
             this.SplitContainer.Panel2.SuspendLayout();
             this.SplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CommandTextTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CommandTextTextBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // Panel
@@ -67,12 +72,14 @@
             // 
             this.SplitContainer.Panel1.Controls.Add(this.CommandTextTextBox1);
             this.SplitContainer.Panel1.Controls.Add(this.ConnectionStringTextBox1);
+            this.SplitContainer.Panel1.Controls.Add(this.label3);
             this.SplitContainer.Panel1.Controls.Add(this.label1);
             // 
             // SplitContainer.Panel2
             // 
             this.SplitContainer.Panel2.Controls.Add(this.CommandTextTextBox2);
             this.SplitContainer.Panel2.Controls.Add(this.ConnectionStringTextBox2);
+            this.SplitContainer.Panel2.Controls.Add(this.label4);
             this.SplitContainer.Panel2.Controls.Add(this.label2);
             this.SplitContainer.Size = new System.Drawing.Size(1046, 681);
             this.SplitContainer.SplitterDistance = 523;
@@ -80,13 +87,42 @@
             // 
             // CommandTextTextBox1
             // 
+            this.CommandTextTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CommandTextTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.CommandTextTextBox1.AutoIndentCharsPatterns = "";
+            this.CommandTextTextBox1.AutoScrollMinSize = new System.Drawing.Size(25, 15);
+            this.CommandTextTextBox1.BackBrush = null;
             this.CommandTextTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.CommandTextTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CommandTextTextBox1.Location = new System.Drawing.Point(2, 44);
+            this.CommandTextTextBox1.CharHeight = 15;
+            this.CommandTextTextBox1.CharWidth = 7;
+            this.CommandTextTextBox1.CommentPrefix = "--";
+            this.CommandTextTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.CommandTextTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.CommandTextTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.CommandTextTextBox1.IsReplaceMode = false;
+            this.CommandTextTextBox1.Language = FastColoredTextBoxNS.Language.SQL;
+            this.CommandTextTextBox1.LeftBracket = '(';
+            this.CommandTextTextBox1.Location = new System.Drawing.Point(2, 56);
             this.CommandTextTextBox1.Name = "CommandTextTextBox1";
-            this.CommandTextTextBox1.Size = new System.Drawing.Size(514, 634);
+            this.CommandTextTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.CommandTextTextBox1.RightBracket = ')';
+            this.CommandTextTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.CommandTextTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("CommandTextTextBox1.ServiceColors")));
+            this.CommandTextTextBox1.Size = new System.Drawing.Size(514, 622);
             this.CommandTextTextBox1.TabIndex = 2;
-            this.CommandTextTextBox1.Text = "";
+            this.CommandTextTextBox1.Zoom = 100;
             // 
             // ConnectionStringTextBox1
             // 
@@ -108,13 +144,42 @@
             // 
             // CommandTextTextBox2
             // 
+            this.CommandTextTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CommandTextTextBox2.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.CommandTextTextBox2.AutoIndentCharsPatterns = "";
+            this.CommandTextTextBox2.AutoScrollMinSize = new System.Drawing.Size(25, 15);
+            this.CommandTextTextBox2.BackBrush = null;
             this.CommandTextTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.CommandTextTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CommandTextTextBox2.Location = new System.Drawing.Point(2, 44);
+            this.CommandTextTextBox2.CharHeight = 15;
+            this.CommandTextTextBox2.CharWidth = 7;
+            this.CommandTextTextBox2.CommentPrefix = "--";
+            this.CommandTextTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.CommandTextTextBox2.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.CommandTextTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.CommandTextTextBox2.IsReplaceMode = false;
+            this.CommandTextTextBox2.Language = FastColoredTextBoxNS.Language.SQL;
+            this.CommandTextTextBox2.LeftBracket = '(';
+            this.CommandTextTextBox2.Location = new System.Drawing.Point(2, 56);
             this.CommandTextTextBox2.Name = "CommandTextTextBox2";
-            this.CommandTextTextBox2.Size = new System.Drawing.Size(514, 634);
+            this.CommandTextTextBox2.Paddings = new System.Windows.Forms.Padding(0);
+            this.CommandTextTextBox2.RightBracket = ')';
+            this.CommandTextTextBox2.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.CommandTextTextBox2.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("CommandTextTextBox2.ServiceColors")));
+            this.CommandTextTextBox2.Size = new System.Drawing.Size(514, 622);
             this.CommandTextTextBox2.TabIndex = 2;
-            this.CommandTextTextBox2.Text = "";
+            this.CommandTextTextBox2.Zoom = 100;
             // 
             // ConnectionStringTextBox2
             // 
@@ -145,6 +210,24 @@
             this.ExecuteButton.UseVisualStyleBackColor = true;
             this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Command 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Command 2";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -165,6 +248,8 @@
             this.SplitContainer.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
             this.SplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CommandTextTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CommandTextTextBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -180,6 +265,8 @@
         private System.Windows.Forms.Label label2;
         private FastColoredTextBoxNS.FastColoredTextBox CommandTextTextBox1;
         private FastColoredTextBoxNS.FastColoredTextBox CommandTextTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
