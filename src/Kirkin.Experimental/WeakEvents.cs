4e2558a689866e08c33ce032159606f961b67f65
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-06-06 17:06:29 +1000

    Experimental: WeakEvents

---
+++ src/Kirkin.Experimental/WeakEvents.cs
@@ -0,0 +1,43 @@
+﻿#if !NET_40
+
+using System;
+
+namespace Kirkin
+{
+    /// <summary>
+    /// Utilities for hooking up event handlers that
+    /// don't interfere with natural garbage collection.
+    /// </summary>
+    public static class WeakEvents
+    {
+        /// <summary>
+        /// Hooks up an event handler which does not prevent the dependency from being collected.
+        /// </summary>
+        public static void HookUpWeakHandlerAction<TDependency>(TDependency dependency, Action<Action> addHandler, Action<Action> removeHandler, Action<TDependency> eventHandler)
+            where TDependency : class
+        {
+            if (dependency == null) throw new ArgumentNullException(nameof(dependency));
+
+            WeakReference<TDependency> weakRef = new WeakReference<TDependency>(dependency);
+            Action finalEventHandler = null;
+
+            finalEventHandler = () =>
+            {
+                TDependency strongRef = null;
+
+                if (weakRef.TryGetTarget(out strongRef))
+                {
+                    eventHandler(strongRef);
+                }
+                else
+                {
+                    removeHandler(finalEventHandler);
+                }
+            };
+
+            addHandler(finalEventHandler);
+        }
+    }
+}
+
+#endif
