885f329a40c0cd578621ec1e408f45aa3639b6d2
Kirill <kirill.shlenskiy@gmail.com>
2016-06-14 20:16:03 +1000

    Code injection: rough API

--- src/Kirkin.Tests/Scratchpad.cs
+++ src/Kirkin.Tests/Scratchpad.cs
@@ -26,5 +26,41 @@ namespace Kirkin.Tests
                 .Create(output.WriteLine)
                 .WithFormatters(EntryFormatter.TimestampNonEmptyEntries());
         }
+
+        public void Inj()
+        {
+            InjectorBuilder<IDummy> builder = new InjectorBuilder<IDummy>();
+
+            builder.SubstituteFunc<int>(
+                d => d.GetValue, func => () => func() + 1
+            );
+
+            builder.SubstituteFunc<int, int>(
+                d => d.AddThree(0), func => i => func(i)
+            );
+
+            IInjector<IDummy> injector = builder.BuildInjector();
+            IDummy dummy = injector.Inject(new Dummy());
+
+            Assert.Equal(42, dummy.GetValue());
+        }
+
+        interface IDummy
+        {
+            int GetValue();
+        }
+
+        sealed class Dummy : IDummy
+        {
+            public int GetValue()
+            {
+                return 42;
+            }
+
+            public int AddThree(int i)
+            {
+                return i + 3;
+            }
+        }
     }
 }
