b071003acc44155e5f2ab906b7f024b99b2755ce
Kirill <kirill.shlenskiy@gmail.com>
2016-06-14 21:37:03 +1000

    Experimental: Kirkin.Functional.Decorate

---
+++ src/Kirkin.Experimental/Functional/Decorate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Kirkin.Functional
+{
+    /// <summary>
+    /// Functional decorators.
+    /// </summary>
+    /// <example>
+    /// Basic usage:
+    /// <code>
+    /// Func&lt;int, int&gt; funcPlusOne = Decorate.Func&lt;int, int&gt;(
+    ///     (x, y) => x + y,
+    ///     f => (x, y) => f(x, y) + 1
+    /// );
+    /// </code>
+    /// </example>
+    public static class Decorate
+    {
+        /// <summary>
+        /// Decorates the given action.
+        /// </summary>
+        public static Action Action(Action action, Func<Action, Action> decorator)
+        {
+            return decorator(action);
+        }
+
+        /// <summary>
+        /// Decorates the given action.
+        /// </summary>
+        public static Action<T> Action<T>(Action<T> action, Func<Action<T>, Action<T>> decorator)
+        {
+            return decorator(action);
+        }
+
+        /// <summary>
+        /// Decorates the given action.
+        /// </summary>
+        public static Action<T1, T2> Action<T1, T2>(Action<T1, T2> action, Func<Action<T1, T2>, Action<T1, T2>> decorator)
+        {
+            return decorator(action);
+        }
+
+        /// <summary>
+        /// Decorates the given action.
+        /// </summary>
+        public static Action<T1, T2, T3> Action<T1, T2, T3>(Action<T1, T2, T3> action, Func<Action<T1, T2, T3>, Action<T1, T2, T3>> decorator)
+        {
+            return decorator(action);
+        }
+
+        /// <summary>
+        /// Decorates the given action.
+        /// </summary>
+        public static Action<T1, T2, T3, T4> Action<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action, Func<Action<T1, T2, T3, T4>, Action<T1, T2, T3, T4>> decorator)
+        {
+            return decorator(action);
+        }
+
+        /// <summary>
+        /// Decorates the given function.
+        /// </summary>
+        public static Func<T> Func<T>(Func<T> func, Func<Func<T>, Func<T>> decorator)
+        {
+            return decorator(func);
+        }
+
+        /// <summary>
+        /// Decorates the given function.
+        /// </summary>
+        public static Func<T, TResult> Func<T, TResult>(Func<T, TResult> func, Func<Func<T, TResult>, Func<T, TResult>> decorator)
+        {
+            return decorator(func);
+        }
+
+        /// <summary>
+        /// Decorates the given function.
+        /// </summary>
+        public static Func<T1, T2, TResult> Func<T1, T2, TResult>(Func<T1, T2, TResult> func, Func<Func<T1, T2, TResult>, Func<T1, T2, TResult>> decorator)
+        {
+            return decorator(func);
+        }
+
+        /// <summary>
+        /// Decorates the given function.
+        /// </summary>
+        public static Func<T1, T2, T3, TResult> Func<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, Func<Func<T1, T2, T3, TResult>, Func<T1, T2, T3, TResult>> decorator)
+        {
+            return decorator(func);
+        }
+
+        /// <summary>
+        /// Decorates the given function.
+        /// </summary>
+        public static Func<T1, T2, T3, T4, TResult> Func<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> func, Func<Func<T1, T2, T3, T4, TResult>, Func<T1, T2, T3, T4, TResult>> decorator)
+        {
+            return decorator(func);
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Functional/DecorateTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Kirkin.Functional;
+
+using Xunit;
+
+namespace Kirkin.Tests.Functional
+{
+    public class DecorateTests
+    {
+        [Fact]
+        public void Api()
+        {
+            Func<int, int> get = i => i;
+            Func<Func<int, int>, Func<int, int>> plusOneDecorator = f => arg => f(arg) + 1;
+
+            Dummy dummy = new Dummy();
+
+            Func<int, int> getValuePlusOne = plusOneDecorator(dummy.AddThree);
+
+            getValuePlusOne = Decorate.Func<int, int>(dummy.AddThree, f => arg => f(arg) + 1);
+
+            getValuePlusOne(123);
+
+            Func<int, int, int> add = (x, y) => x + y;
+            Func<Func<int, int, int>, Func<int, int, int>> plusOneDecorator2 = f => (x, y) => f(x, y) + 1;
+            Action<int> consume = i => { };
+
+            Action<int> consumeRetry = Decorate.Action<int>(i => { }, action => i =>
+            {
+                try
+                {
+                    action(i);
+                }
+                catch
+                {
+                    action(i);
+                }
+            });
+
+            Func<Action<int>, Action<int>> retryDecorator = f => i =>
+            {
+                try
+                {
+                    f(i);
+                }
+                catch
+                {
+                    f(i);
+                }
+            };
+        }
+
+        sealed class Dummy
+        {
+            public int AddThree(int i)
+            {
+                return i + 3;
+            }
+        }
+    }
+}
