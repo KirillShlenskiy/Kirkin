1fb99acf08d6836952d181436cec9e3946956ec3
Kirill <kirill.shlenskiy@gmail.com>
2016-07-09 22:33:01 +1000

    Experimental: Kirkin.Reflection.Method type, provisional test

---
+++ src/Kirkin.Experimental/Reflection/Method.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kirkin.Reflection
+{
+    public sealed class Method
+    {
+        public static Method InstanceMethod<T>(Expression<Action<T>> methodCall)
+        {
+            MethodCallExpression call = methodCall.Body as MethodCallExpression;
+
+            if (call == null) {
+                throw new InvalidOperationException($"The given expression is not a {nameof(MethodCallExpression)}");
+            }
+
+            return new Method(methodCall, call);
+        }
+
+        public static Method StaticMethod(Expression<Action> methodCall)
+        {
+            MethodCallExpression call = methodCall.Body as MethodCallExpression;
+
+            if (call == null) {
+                throw new InvalidOperationException($"The given expression is not a {nameof(MethodCallExpression)}");
+            }
+
+            return new Method(methodCall, call);
+        }
+
+        private readonly LambdaExpression Lambda;
+        private readonly MethodCallExpression MethodCall;
+
+        private Method(LambdaExpression lambda, MethodCallExpression methodCall)
+        {
+            Lambda = lambda;
+            MethodCall = methodCall;
+        }
+
+        public MethodCallExpression Expression
+        {
+            get
+            {
+                return MethodCall;
+            }
+        }
+
+        public MethodInfo MethodInfo
+        {
+            get
+            {
+                return MethodCall.Method;
+            }
+        }
+
+        public TDelegate CreateDelegate<TDelegate>()
+        {
+            return (TDelegate)(object)Delegate.CreateDelegate(typeof(TDelegate), MethodInfo);
+        }
+
+        public TDelegate CreateDelegate<TDelegate>(object instance)
+        {
+            return (TDelegate)(object)Delegate.CreateDelegate(typeof(TDelegate), instance, MethodInfo);
+        }
+
+        public Expression<TDelegate> ToDelegate<TDelegate>()
+        {
+            return System.Linq.Expressions.Expression.Lambda<TDelegate>(MethodCall, Lambda.Parameters);
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Reflection/MethodTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Kirkin.Reflection;
+
+using Xunit;
+
+namespace Kirkin.Tests.Reflection
+{
+    public class MethodTests
+    {
+        [Fact(Skip = "TODO")]
+        public void CreateDelegateFromGenericExpression()
+        {
+            Func<string, string> func = Method
+                .StaticMethod(() => ContainerType.Process(123))
+                .CreateDelegate<Func<string, string>>();
+
+            Assert.Equal("zzz", func("zzz"));
+        }
+
+        static class ContainerType
+        {
+            public static T Process<T>(T value)
+            {
+                return value;
+            }
+        }
+    }
+}
