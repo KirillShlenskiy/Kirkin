8659907dc2f1bb0bcbd52d61eacc694692f4a3e4
Kirill <kirill.shlenskiy@gmail.com>
2016-07-16 01:28:07 +1000

    MapperBuilderExtensions type

---
+++ src/Kirkin/Mapping/MapperBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Kirkin.Mapping
+{
+    /// <summary>
+    /// Fluent <see cref="MapperBuilder{TSource, TTarget}"/> extensions.
+    /// </summary>
+    internal static class MapperBuilderExtensions
+    {
+        /// <summary>
+        /// Executes the given configuration action on this
+        /// builder instance and returns the mutated instance.
+        /// </summary>
+        public static MapperBuilder<TSource, TTarget> Configure<TSource, TTarget>(this MapperBuilder<TSource, TTarget> builder,
+                                                                                  Action<MapperBuilder<TSource, TTarget>> configureAction)
+        {
+            configureAction(builder);
+
+            return builder;
+        }
+    }
+}
