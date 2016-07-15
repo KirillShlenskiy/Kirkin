1d81ab564c5f20d7d23f48039586c078732bc154
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-07-15 18:51:06 +1000

    MapperBuilderFactory placeholder

---
+++ src/Kirkin/Mapping/MapperBuilderFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Kirkin.Mapping.Engine;
+
+namespace Kirkin.Mapping
+{
+    internal struct MapperBuilderFactory<TSource>
+    {
+        private readonly Member[] SourceMembers;
+
+        internal MapperBuilderFactory(Member[] sourceMembers) // Pass in Func<MapperBuilder<TSource, TFactory>> where TSource is pre-set?
+        {
+            if (sourceMembers == null) throw new ArgumentNullException(nameof(sourceMembers));
+
+            SourceMembers = sourceMembers;
+        }
+
+        public MapperBuilder<TSource, TTarget> ToObject<TTarget>()
+        {
+            Member[] targetMembers = PropertyMember.PublicInstanceProperties<TTarget>();
+
+            return new MapperBuilder<TSource, TTarget>(SourceMembers, targetMembers);
+        }
+    }
+}
