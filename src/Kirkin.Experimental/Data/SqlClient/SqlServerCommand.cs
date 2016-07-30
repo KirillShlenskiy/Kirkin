795e92fe6aff0b0779077544ccaf5b3c2ffb0ea4
Kirill <kirill.shlenskiy@gmail.com>
2016-07-30 16:50:29 +1000

    SqlServerConnectionFactory provisions

---
+++ src/Kirkin.Experimental/Data/SqlClient/SqlServerCommand.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Kirkin.Data.SqlClient
+//{
+//    /// <summary>
+//    /// Type which abstracts away connection and command management.
+//    /// </summary>
+//    public sealed class SqlServerCommand
+//    {
+//        public string ConnectionString
+//        {
+//            get
+//            {
+//                return ConnectionFactory.ConnectionString;
+//            }
+//        }
+
+//        private readonly SqlServerConnectionFactory ConnectionFactory;
+//        private readonly bool OwnsConnectionFactory;
+
+//        public SqlServerCommand(string connectionString)
+//            : this(new SqlServerConnectionFactory(connectionString))
+//        {
+//        }
+
+//        public SqlServerCommand(SqlServerConnectionFactory connectionFactory)
+//        {
+//            if (connectionFactory == null) throw new ArgumentNullException(nameof(connectionFactory));
+
+//            ConnectionFactory = connectionFactory;
+//        }
+
+//        private SqlServerCommand(SqlServerConnectionFactory connectionFactory, bool ownsConnectionFactory)
+//        {
+//            ConnectionFactory = connectionFactory;
+//            OwnsConnectionFactory = ownsConnectionFactory;
+//        }
+//    }
+//}
