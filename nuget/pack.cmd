adaf4095f66c07f471be7c68b1e17348988ccc05
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-08-03 17:40:44 +1000

    More pack.cmd changes

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,3 +1,4 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Clean /verbosity:minimal
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Rebuild /verbosity:minimal
-nuget pack Kirkin.nuspec -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU
+nuget pack Kirkin.nuspec -IncludeReferencedProjects -Properties Configuration=Release
 nuget pack Kirkin.Sources.nuspec -IncludeReferencedProjects
