a575bab89976aa2aa32cfd9722ad56d2a19b891c
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-08-04 11:03:27 +1000

    pack.cmd: exit on error

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,4 +1,3 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Clean /verbosity:minimal
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Rebuild /verbosity:minimal
-nuget pack Kirkin.nuspec -IncludeReferencedProjects -Properties Configuration=Release
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Rebuild /verbosity:minimal || exit /b
+nuget pack Kirkin.nuspec -IncludeReferencedProjects -Properties Configuration=Release || exit /b
 nuget pack Kirkin.Sources.nuspec -IncludeReferencedProjects
