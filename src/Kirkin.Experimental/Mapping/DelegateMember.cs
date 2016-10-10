26730bd7065b6ac41cdee82077893b8308c7e1f1
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-10-10 09:11:52 +0900

    Mapping (experimental): DelegateMember provisions, tests (failing)

---
+++ src/Kirkin.Experimental/Mapping/DelegateMember.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kirkin.Mapping
+{
+    internal sealed class DelegateMember<TObject, TValue>
+        : Member<TObject>
+    {
+        private readonly Func<TObject, TValue> _getter;
+        private readonly Action<TObject, TValue> _setter;
+
+        public override bool CanRead
+        {
+            get
+            {
+                return _getter != null;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return _setter != null;
+            }
+        }
+
+        public override string Name { get; }
+
+        public override Type Type
+        {
+            get
+            {
+                return typeof(TValue);
+            }
+        }
+
+        public DelegateMember(string name, Func<TObject, TValue> getter)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (getter == null) throw new ArgumentNullException(nameof(getter));
+
+            Name = name;
+            _getter = getter;
+        }
+
+        public DelegateMember(string name, Action<TObject, TValue> setter)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (setter == null) throw new ArgumentNullException(nameof(setter));
+
+            Name = name;
+            _setter = setter;
+        }
+
+        public DelegateMember(string name, Func<TObject, TValue> getter, Action<TObject, TValue> setter)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (getter == null) throw new ArgumentNullException(nameof(getter));
+            if (setter == null) throw new ArgumentNullException(nameof(setter));
+
+            Name = name;
+            _getter = getter;
+            _setter = setter;
+        }
+
+        protected internal override Expression ResolveGetter(ParameterExpression source)
+        {
+            if (!CanRead){
+                throw new NotSupportedException($"This {nameof(DelegateMember<TObject, TValue>)} does not provide a getter.");
+            }
+
+            MethodInfo invokeMethod = typeof(Func<TObject, TValue>).GetMethod("Invoke");
+
+            return Expression.Call(Expression.Constant(_getter), invokeMethod, source);
+        }
+
+        protected internal override Expression ResolveSetter(ParameterExpression target)
+        {
+            if (!CanWrite){
+                throw new NotSupportedException($"This {nameof(DelegateMember<TObject, TValue>)} does not provide a setter.");
+            }
+
+            MethodInfo invokeMethod = typeof(Action<TObject, TValue>).GetMethod("Invoke");
+
+            return Expression.Call(Expression.Constant(_getter), invokeMethod, target);
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Mapping/DelegateMemberMappingTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+using Kirkin.Mapping;
+
+using Xunit;
+
+namespace Kirkin.Tests.Mapping
+{
+    public class DelegateMemberMappingTests
+    {
+        [Fact]
+        public void Getter()
+        {
+            // Getter only.
+            DelegateMember<Dictionary<string, object>, int> idMember
+                = new DelegateMember<Dictionary<string, object>, int>("ID", dict => (int)dict["ID"]);
+
+            // Getter and setter.
+            DelegateMember<Dictionary<string, object>, string> valueMember
+                = new DelegateMember<Dictionary<string, object>, string>("Value", dict => (string)dict["Value"], (dict, value) => dict["Value"] = value);
+
+            Mapper<Dictionary<string, object>, Dummy> mapper = Mapper.Builder
+                .From(new Member<Dictionary<string, object>>[] { idMember, valueMember })
+                .To<Dummy>()
+                .BuildMapper();
+
+            Dictionary<string, object> values = new Dictionary<string, object> {
+                { "ID", 123 },
+                { "Value", "Test" }
+            };
+
+            Dummy d = mapper.Map(values);
+
+            Assert.Equal(123, d.ID);
+            Assert.Equal("Test", d.Value);
+        }
+
+        [Fact]
+        public void Setter()
+        {
+
+            // Getter only.
+            DelegateMember<Dictionary<string, object>, int> idMember
+                = new DelegateMember<Dictionary<string, object>, int>("ID", (dict, id) => dict["ID"] = id);
+
+            // Getter and setter.
+            DelegateMember<Dictionary<string, object>, string> valueMember
+                = new DelegateMember<Dictionary<string, object>, string>("Value", dict => (string)dict["Value"], (dict, value) => dict["Value"] = value);
+
+            Mapper<Dummy, Dictionary<string, object>> mapper = Mapper.Builder
+                .From<Dummy>()
+                .To(new Member<Dictionary<string, object>>[] { idMember, valueMember })
+                .BuildMapper();
+
+            Dummy d = new Dummy {
+                ID = 123,
+                Value = "Test"
+            };
+
+            // Default Dictionary constructor will be used.
+            Dictionary<string, object> values = mapper.Map(d);
+
+            Assert.Equal(2, values.Count);
+        }
+
+        sealed class Dummy
+        {
+            public int ID { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
