ab2447d46ed87a40576101b94fd55f67f286fba8
Kirill Shlenskiy <kirill@ardex.com.au>
2016-10-15 11:34:29 +0900

    Dynamically converted ExpressionMember<TObject> type

--- src/Kirkin.Experimental/Mapping/ExpressionMember.cs
+++ src/Kirkin.Experimental/Mapping/ExpressionMember.cs
@@ -58,4 +58,58 @@ namespace Kirkin.Mapping
             throw new NotSupportedException($"This {nameof(ExpressionMember<TObject, TValue>)} does not provide a setter.");
         }
     }
+
+    /// <summary>
+    /// Read-only expression-based <see cref="Member{T}"/> implementation.
+    /// </summary>
+    internal sealed class ExpressionMember<TObject>
+        : Member<TObject>
+    {
+        private readonly Expression<Func<TObject, object>> Getter;
+
+        public override bool CanRead
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override string Name { get; }
+
+        public override Type Type
+        {
+            get
+            {
+                return typeof(object);
+            }
+        }
+
+        public ExpressionMember(string name, Expression<Func<TObject, object>> getter)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (getter == null) throw new ArgumentNullException(nameof(getter));
+
+            Name = name;
+            Getter = getter;
+        }
+
+        protected internal override Expression ResolveGetter(ParameterExpression source)
+        {
+            return new SubstituteParameterVisitor(source).Visit(Getter.Body);
+        }
+
+        protected internal override Expression ResolveSetter(ParameterExpression target)
+        {
+            throw new NotSupportedException($"This {nameof(ExpressionMember<TObject>)} does not provide a setter.");
+        }
+    }
 }
--- src/Kirkin.Tests/Mapping/ExpressionMemberMappingTests.cs
+++ src/Kirkin.Tests/Mapping/ExpressionMemberMappingTests.cs
@@ -9,10 +9,32 @@ namespace Kirkin.Tests.Mapping
     public class ExpressionMemberMappingTests
     {
         [Fact]
-        public void Getter()
+        public void GenericGetter()
         {
             Member<Dictionary<string, object>> idMember = new ExpressionMember<Dictionary<string, object>, int>("ID", dict => (int)dict["ID"]);
             Member<Dictionary<string, object>> valueMember = new ExpressionMember<Dictionary<string, object>, string>("Value", dict => (string)dict["Value"]);
+
+            Mapper<Dictionary<string, object>, Dummy> mapper = Mapper.Builder
+                .From(new[] { idMember, valueMember })
+                .To<Dummy>()
+                .BuildMapper();
+
+            Dictionary<string, object> values = new Dictionary<string, object> {
+                { "ID", 123 },
+                { "Value", "Test" }
+            };
+
+            Dummy d = mapper.Map(values);
+
+            Assert.Equal(123, d.ID);
+            Assert.Equal("Test", d.Value);
+        }
+
+        [Fact]
+        public void NonGenericGetter()
+        {
+            Member<Dictionary<string, object>> idMember = new ExpressionMember<Dictionary<string, object>>("ID", dict => (int)dict["ID"]);
+            Member<Dictionary<string, object>> valueMember = new ExpressionMember<Dictionary<string, object>>("Value", dict => (string)dict["Value"]);
 
             Mapper<Dictionary<string, object>, Dummy> mapper = Mapper.Builder
                 .From(new[] { idMember, valueMember })
@@ -54,10 +76,33 @@ namespace Kirkin.Tests.Mapping
         }
 
         [Fact]
-        public void ExpressionMappingBenchmark()
+        public void GenericExpressionMappingBenchmark()
         {
             Member<Dictionary<string, object>> idMember = new ExpressionMember<Dictionary<string, object>, int>("ID", dict => (int)dict["ID"]);
             Member<Dictionary<string, object>> valueMember = new ExpressionMember<Dictionary<string, object>, string>("Value", dict => (string)dict["Value"]);
+
+            Mapper<Dictionary<string, object>, Dummy> mapper = Mapper.Builder
+                .From(new[] { idMember, valueMember })
+                .To<Dummy>()
+                .BuildMapper();
+
+            Dictionary<string, object> values = new Dictionary<string, object> {
+                { "ID", 123 },
+                { "Value", "Test" }
+            };
+
+            Dummy d = new Dummy();
+
+            for (int i = 0; i < 5000000; i++) {
+                mapper.Map(values, d);
+            }
+        }
+
+        [Fact]
+        public void NonGenericExpressionMappingBenchmark()
+        {
+            Member<Dictionary<string, object>> idMember = new ExpressionMember<Dictionary<string, object>>("ID", dict => (int)dict["ID"]);
+            Member<Dictionary<string, object>> valueMember = new ExpressionMember<Dictionary<string, object>>("Value", dict => (string)dict["Value"]);
 
             Mapper<Dictionary<string, object>, Dummy> mapper = Mapper.Builder
                 .From(new[] { idMember, valueMember })
