82513cbe6995b8a997b1d42b8ccd4ef8e10db743
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-11-22 15:17:13 +1100

    Kirkin.Linq.Expressions.Fluent namespace for member resolution helpers

---
+++ src/Kirkin.Experimental/Linq/Expressions/Fluent/ConstructorResolutionHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Kirkin.Collections.Generic;
+
+namespace Kirkin.Linq.Expressions.Fluent
+{
+    /// <summary>
+    /// Instance constructor expression resolution helper type.
+    /// </summary>
+    public sealed class ConstructorResolutionHelper<T>
+    {
+        internal static readonly ConstructorResolutionHelper<T> Instance = new ConstructorResolutionHelper<T>();
+
+        private ConstructorResolutionHelper()
+        {
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the
+        /// parameterless instance constructor of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public constructors can be matched.</param>
+        public Expression<Func<T>> Parameterless(bool nonPublic = false)
+        {
+            return ConstructorWithGivenParameters<Func<T>>(Array<Type>.Empty, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// constructor of type <typeparamref name="T"/> with the given parameter.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public constructors can be matched.</param>
+        public Expression<Func<TParam, T>> WithParameters<TParam>(bool nonPublic = false)
+        {
+            return ConstructorWithGivenParameters<Func<TParam, T>>(new[] { typeof(TParam) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// constructor of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public constructors can be matched.</param>
+        public Expression<Func<TParam1, TParam2, T>> WithParameters<TParam1, TParam2>(bool nonPublic = false)
+        {
+            return ConstructorWithGivenParameters<Func<TParam1, TParam2, T>>(new[] { typeof(TParam1), typeof(TParam2) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// constructor of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public constructors can be matched.</param>
+        public Expression<Func<TParam1, TParam2, TParam3, T>> WithParameters<TParam1, TParam2, TParam3>(bool nonPublic = false)
+        {
+            return ConstructorWithGivenParameters<Func<TParam1, TParam2, TParam3, T>>(
+                new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// constructor of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public constructors can be matched.</param>
+        public Expression<Func<TParam1, TParam2, TParam3, TParam4, T>> WithParameters<TParam1, TParam2, TParam3, TParam4>(bool nonPublic = false)
+        {
+            return ConstructorWithGivenParameters<Func<TParam1, TParam2, TParam3, TParam4, T>>(
+                new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) }, nonPublic);
+        }
+
+        private static Expression<TDelegate> ConstructorWithGivenParameters<TDelegate>(Type[] parameterTypes, bool nonPublic)
+        {
+            BindingFlags bindingFlags = nonPublic
+                ? BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                : BindingFlags.Instance | BindingFlags.Public;
+
+            ConstructorInfo constructor = typeof(T).GetConstructor(bindingFlags, null, parameterTypes, null);
+
+            if (constructor == null) {
+                throw new InvalidOperationException("Unable to resolve the constructor with matching parameters.");
+            }
+
+            return ExpressionEngine.Constructor<TDelegate>(constructor);
+        }
+    }
+}
---
+++ src/Kirkin.Experimental/Linq/Expressions/Fluent/MethodResolutionHelpercs.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Kirkin.Collections.Generic;
+
+namespace Kirkin.Linq.Expressions.Fluent
+{
+    /// <summary>
+    /// Instance method expression resolution helper type.
+    /// </summary>
+    public sealed class MethodResolutionHelper<T>
+    {
+        internal static readonly MethodResolutionHelper<T> Instance = new MethodResolutionHelper<T>();
+
+        private MethodResolutionHelper()
+        {
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with no parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Action<T>> Void(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Action<T>>(name, Array<Type>.Empty, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Action<T, TParam>> Void<TParam>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Action<T, TParam>>(name, new[] { typeof(TParam) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Action<T, TParam1, TParam2>> Void<TParam1, TParam2>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Action<T, TParam1, TParam2>>(name, new[] { typeof(TParam1), typeof(TParam2) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Action<T, TParam1, TParam2, TParam3>> Void<TParam1, TParam2, TParam3>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Action<T, TParam1, TParam2, TParam3>>(
+                name, new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Action<T, TParam1, TParam2, TParam3, TParam4>> Void<TParam1, TParam2, TParam3, TParam4>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Action<T, TParam1, TParam2, TParam3, TParam4>>(
+                name, new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with no parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Func<T, TReturn>> Func<TReturn>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Func<T, TReturn>>(name, Array<Type>.Empty, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Func<T, TParam, TReturn>> Func<TParam, TReturn>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Func<T, TParam, TReturn>>(name, new[] { typeof(TParam) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Func<T, TParam1, TParam2, TReturn>> Func<TParam1, TParam2, TReturn>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Func<T, TParam1, TParam2, TReturn>>(name, new[] { typeof(TParam1), typeof(TParam2) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Func<T, TParam1, TParam2, TParam3, TReturn>> Func<TParam1, TParam2, TParam3, TReturn>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Func<T, TParam1, TParam2, TParam3, TReturn>>(
+                name, new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) }, nonPublic);
+        }
+
+        /// <summary>
+        /// Creates an expression which represents the invocation of the instance
+        /// method of type <typeparamref name="T"/> with the given parameters.
+        /// </summary>
+        /// <param name="nonPublic">True if non-public methods can be matched.</param>
+        public Expression<Func<T, TParam1, TParam2, TParam3, TParam4, TReturn>> Func<TParam1, TParam2, TParam3, TParam4, TReturn>(string name, bool nonPublic = false)
+        {
+            return InstanceMethod<Func<T, TParam1, TParam2, TParam3, TParam4, TReturn>>(
+                name, new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) }, nonPublic);
+        }
+
+        private static Expression<TDelegate> InstanceMethod<TDelegate>(string name, Type[] parameterTypes, bool nonPublic)
+        {
+            BindingFlags bindingFlags = nonPublic
+                ? BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                : BindingFlags.Instance | BindingFlags.Public;
+
+            MethodInfo method = typeof(T).GetMethod(name, bindingFlags, null, parameterTypes, null);
+
+            if (method == null) {
+                throw new InvalidOperationException("Unable to resolve instance method with matching parameters.");
+            }
+
+            return ExpressionEngine.Method<TDelegate>(method);
+        }
+    }
+}
