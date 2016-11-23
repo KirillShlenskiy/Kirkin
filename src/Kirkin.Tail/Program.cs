66b395ff7b8a435f206860541a9ccf521dd79fb4
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-11-23 14:59:21 +1100

    Kirkin.Tail utility

---
+++ src/Kirkin.Tail/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+using Kirkin.Monitoring;
+
+namespace Kirkin.Tail
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+                AssemblyName assemblyInfo = executingAssembly.GetName();
+
+                Console.WriteLine($"Real-time log reader utility v{assemblyInfo.Version}. Usage: tail <filename/path>.");
+            }
+            else if (args.Length > 1)
+            {
+                Console.WriteLine("Only one arg is supported (file name/path).");
+            }
+            else
+            {
+                string filePath = args[0];
+                ReactiveFileMonitor monitor = new ReactiveFileMonitor(filePath);
+
+                monitor.LineRead += Console.WriteLine;
+
+                monitor.MonitorAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
