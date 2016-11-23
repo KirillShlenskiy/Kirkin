f5ab070cfc88c33ccf81d65ba016388d25722b77
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-11-23 15:05:41 +1100

    Tail: Fody.Costura, proper implementation

--- src/Kirkin.Tail/Program.cs
+++ src/Kirkin.Tail/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 using Kirkin.Monitoring;
@@ -7,27 +8,62 @@ namespace Kirkin.Tail
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            if (args.Length == 0)
+            try
             {
-                Assembly executingAssembly = Assembly.GetExecutingAssembly();
-                AssemblyName assemblyInfo = executingAssembly.GetName();
+                if (args.Length == 0)
+                {
+                    Assembly executingAssembly = Assembly.GetExecutingAssembly();
+                    AssemblyName assemblyInfo = executingAssembly.GetName();
 
-                Console.WriteLine($"Real-time log reader utility v{assemblyInfo.Version}. Usage: tail <filename/path>.");
+                    Console.WriteLine($"Real-time log reader utility v{assemblyInfo.Version}. Usage: tail <filename/path>.");
+
+                    return 0;
+                }
+                else if (args.Length > 1)
+                {
+                    Console.WriteLine("Only one arg is supported (file name/path).");
+
+                    return -1;
+                }
+                else
+                {
+                    string filePath = args[0];
+                    FileInfo file = new FileInfo(filePath);
+
+                    if (!file.Exists)
+                    {
+                        Console.WriteLine($"File '{file.FullName}' does not exist.");
+
+                        return -1;
+                    }
+
+                    ReactiveFileMonitor monitor = new ReactiveFileMonitor(file.FullName);
+
+                    monitor.LineRead += Console.WriteLine;
+
+                    monitor.MonitorAsync().GetAwaiter().GetResult();
+
+                    return 0;
+                }
             }
-            else if (args.Length > 1)
+            catch (Exception ex)
             {
-                Console.WriteLine("Only one arg is supported (file name/path).");
-            }
-            else
-            {
-                string filePath = args[0];
-                ReactiveFileMonitor monitor = new ReactiveFileMonitor(filePath);
+                ConsoleColor defaultColor = Console.ForegroundColor;
 
-                monitor.LineRead += Console.WriteLine;
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
 
-                monitor.MonitorAsync().GetAwaiter().GetResult();
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    Console.ForegroundColor = defaultColor;
+                }
+
+                return -1;
             }
         }
     }
