441b713370079776975ca2dc62ebcd93a7b48233
Kirill <kirill.shlenskiy@gmail.com>
2016-11-28 22:16:31 +1100

    Super basic AsyncSemaphore

---
+++ src/Kirkin.Experimental/Threading/AsyncSemaphore.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kirkin.Threading
+{
+    public sealed class AsyncSemaphore
+    {
+        private static readonly Task s_completedTask = Task.FromResult(true);
+
+        private readonly Queue<TaskCompletionSource<bool>> Waiters = new Queue<TaskCompletionSource<bool>>();
+        private int _count;
+
+        public int CurrentCount
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public int MaxCount { get; }
+
+        public AsyncSemaphore(int initialCount)
+            : this(initialCount, int.MaxValue)
+        {
+        }
+
+        public AsyncSemaphore(int initialCount, int maxCount)
+        {
+            _count = initialCount;
+            MaxCount = maxCount;
+        }
+
+        public Task WaitAsync()
+        {
+            lock (Waiters)
+            {
+                if (_count > 0)
+                {
+                    _count--;
+
+                    return s_completedTask;
+                }
+
+                TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+                Waiters.Enqueue(tcs);
+
+                return tcs.Task;
+            }
+        }
+
+        public void Release(int releaseCount = 1)
+        {
+            lock (Waiters)
+            {
+                if (Waiters.Count != 0)
+                {
+                    TaskCompletionSource<bool> tcs = Waiters.Dequeue();
+
+                    tcs.SetResult(true);
+                }
+                else
+                {
+                    _count += releaseCount;
+                }
+            }
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Threading/AsyncSemaphoreTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+using Kirkin.Threading;
+
+using Xunit;
+
+namespace Kirkin.Tests.Threading
+{
+    public class AsyncSemaphoreTests
+    {
+        [Fact]
+        public void BasicApi()
+        {
+            AsyncSemaphore semaphore = new AsyncSemaphore(1, 1);
+
+            Assert.Equal(TaskStatus.RanToCompletion, semaphore.WaitAsync().Status); // Synchronous completion.
+
+            Task t2 = semaphore.WaitAsync();
+
+            Assert.False(t2.IsCompleted);
+
+            Task t3 = semaphore.WaitAsync();
+
+            Assert.False(t3.IsCompleted);
+
+            semaphore.Release();
+
+            Assert.Equal(TaskStatus.RanToCompletion, t2.Status);
+            Assert.False(t3.IsCompleted);
+
+            semaphore.Release();
+
+            Assert.Equal(TaskStatus.RanToCompletion, t3.Status);
+        }
+    }
+}
