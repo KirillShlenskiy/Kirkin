62fd8b547642c8fc2ed2f50be17c8ff377d3a189
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-12-02 12:00:01 +1100

    TaskUtil type

---
+++ src/Kirkin.Experimental/Threading/Tasks/TaskUtil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Kirkin.Linq.Expressions;
+
+namespace Kirkin.Threading.Tasks
+{
+    /// <summary>
+    /// Common <see cref="Task"/> helpers.
+    /// </summary>
+    public static class TaskUtil
+    {
+        private static readonly Task s_completedTask = CreateCompletedTask();
+
+        /// <summary>
+        /// Shared instance of an already completed task.
+        /// </summary>
+        public static Task CompletedTask
+        {
+            get
+            {
+                return s_completedTask;
+            }
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Task"/> in a completed or canceled state.
+        /// </summary>
+        public static Task CreateCompletedTask(bool canceled = false, TaskCreationOptions creationOptions = TaskCreationOptions.None, CancellationToken ct = default(CancellationToken))
+        {
+            return CompletedTaskFactory.Factory(canceled, creationOptions, ct);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Task"/> in a completed or canceled state, with the given result.
+        /// </summary>
+        public static Task<TResult> CreateCompletedTask<TResult>(bool canceled = false, TResult result = default(TResult), TaskCreationOptions creationOptions = TaskCreationOptions.None, CancellationToken ct = default(CancellationToken))
+        {
+            return CompletedTaskFactory<TResult>.Factory(canceled, result, creationOptions, ct);
+        }
+
+        static class CompletedTaskFactory
+        {
+            public static readonly Func<bool, TaskCreationOptions, CancellationToken, Task> Factory = InstanceMemberExpressions
+                .Constructor<Task>()
+                .WithParameters<bool, TaskCreationOptions, CancellationToken>(nonPublic: true)
+                .Compile();
+        }
+
+        static class CompletedTaskFactory<TResult>
+        {
+            public static readonly Func<bool, TResult, TaskCreationOptions, CancellationToken, Task<TResult>> Factory = InstanceMemberExpressions
+                .Constructor<Task<TResult>>()
+                .WithParameters<bool, TResult, TaskCreationOptions, CancellationToken>(nonPublic: true)
+                .Compile();
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Threading/Tasks/TaskUtilTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+using Kirkin.Threading.Tasks;
+
+using Xunit;
+
+namespace Kirkin.Tests.Threading.Tasks
+{
+    public class TaskUtilTests
+    {
+        [Fact]
+        public void CompletedVoidTask()
+        {
+            Task task = TaskUtil.CompletedTask;
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+        }
+    }
+}
