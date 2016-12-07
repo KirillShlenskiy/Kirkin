94f85fcc65dedebe6f9ab253b1f99393d74ea545
Kirill <kirill.shlenskiy@gmail.com>
2016-12-08 01:31:46 +1100

    Kirkin.Decisions: Extensions type

--- src/Kirkin.Tests/Decisions/DecisionTests.cs
+++ src/Kirkin.Tests/Decisions/DecisionTests.cs
@@ -20,9 +20,9 @@ namespace Kirkin.Tests.Decisions
             Output = output;
         }
 
-        sealed class EvenPreference : IPreference<double>
+        sealed class EvenPreference : IPreference<int>
         {
-            public Decision<double> EstimateFitness(double input)
+            public Decision<int> EstimateFitness(int input)
             {
                 return Decision.Create(this, input, input % 2 == 0 ? 1 : 0);
             }
@@ -31,9 +31,9 @@ namespace Kirkin.Tests.Decisions
         [Fact]
         public void DecideSomething()
         {
-            IPreference<double> makeItBig = new HigherIsBetterPreference(0, 10);
-            IPreference<double> evenIsBetter = new EvenPreference();
-            IPreference<double> pref = new CompositePreference<double>("Comp", makeItBig, evenIsBetter);
+            IPreference<int> makeItBig = new HigherIsBetterPreference(0, 10).WithInputConversion((int i) => i);
+            IPreference<int> evenIsBetter = new EvenPreference();
+            IPreference<int> pref = new CompositePreference<int>("Comp", makeItBig, evenIsBetter);
 
             for (int i = 0; i <= 10; i++)
             {
---
+++ src/Kirkin/Decisions/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kirkin.Decisions
+{
+    public static class Extensions
+    {
+        public static IPreference<TInput> WithInputConversion<TInput, TConverted>(this IPreference<TConverted> preference, Func<TInput, TConverted> conversion)
+        {
+            return new ProjectPreference<TInput, TConverted>(preference.ToString(), conversion, preference);
+        }
+    }
+}
