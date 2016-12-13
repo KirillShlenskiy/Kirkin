aaae5c800b0d4b10851561e80e23a6a45a068aaf
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2016-12-13 18:16:18 +1100

    Experimental: MD5Hash

---
+++ src/Kirkin.Experimental/Utilities/MD5Hash.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Kirkin.Utilities
+{
+    /// <summary>
+    /// Encapsulates an MD5 hash.
+    /// </summary>
+    public sealed class MD5Hash
+    {
+        /// <summary>
+        /// Computes the hash value for the file at the given path.
+        /// </summary>
+        public static MD5Hash ComputeFileHash(string filePath)
+        {
+            using (FileStream stream = File.OpenRead(filePath)) {
+                return ComputeHash(stream);
+            }
+        }
+
+        /// <summary>
+        /// Computes the hash value for the given stream.
+        /// </summary>
+        public static MD5Hash ComputeHash(Stream stream)
+        {
+            // MD5 instances are not thread-safe,
+            // so we create a new one on every call.
+            using (MD5 algorithm = MD5.Create())
+            {
+                byte[] hashBytes = algorithm.ComputeHash(stream);
+
+                return new MD5Hash(hashBytes);
+            }
+        }
+
+        /// <summary>
+        /// Byte array representation of the MD5 hash.
+        /// </summary>
+        public byte[] HashBytes { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="MD5Hash"/> instance.
+        /// </summary>
+        private MD5Hash(byte[] hashBytes)
+        {
+            HashBytes = hashBytes;
+        }
+
+        /// <summary>
+        /// Returns the Base64 representation of this instance.
+        /// </summary>
+        public override string ToString()
+        {
+            return BitConverter
+                .ToString(HashBytes)
+                .Replace("-", "")
+                .ToLower();
+        }
+
+        /// <summary>
+        /// Returns the string representation of this instance.
+        /// </summary>
+        public string ToString(int toBase)
+        {
+            return string.Concat(
+                HashBytes.Select(b => Convert.ToString(b, toBase))
+            );
+        }
+    }
+}
