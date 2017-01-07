b4a1bba9a0afc24bda39e13239d9eff625a68999
Kirill <kirill.shlenskiy@gmail.com>
2017-01-07 13:44:06 +1100

    Experimental: FastMethodInvokerTests added

---
+++ src/Kirkin.Tests/Reflection/FastMethodInvokerTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Xunit;
+
+namespace Kirkin.Tests.Reflection
+{
+    public class FastMethodInvokerTests
+    {
+        [Fact]
+        public void TestActionNoArgs()
+        {
+            FastMethodInvoker method = CreateFastMethodInfo("ActionNoArgs");
+
+            Assert.Null(method.Invoke(new Dummy(), null));
+            Assert.Null(method.Invoke(new Dummy(), new int[0]));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, new object[] { 1 }));
+        }
+
+        [Fact]
+        public void TestActionOneArg()
+        {
+            FastMethodInvoker method = CreateFastMethodInfo("ActionOneArg");
+
+            Assert.Null(method.Invoke(new Dummy(), new object[] { 42 }));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, new object[0]));
+        }
+
+        [Fact]
+        public void TestFuncNoArgs()
+        {
+            FastMethodInvoker method = CreateFastMethodInfo("FuncNoArgs");
+
+            Assert.Equal(42, method.Invoke(new Dummy(), null));
+            Assert.Equal(42, method.Invoke(new Dummy(), new int[0]));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, new object[] { 1 }));
+        }
+
+        [Fact]
+        public void TestFuncOneArg()
+        {
+            FastMethodInvoker method = CreateFastMethodInfo("FuncOneArg");
+
+            Assert.Equal(42, method.Invoke(new Dummy(), new object[] { 42 }));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => method.Invoke(null, new object[0]));
+        }
+
+        private static FastMethodInvoker CreateFastMethodInfo(string methodName)
+        {
+            MethodInfo mi = typeof(Dummy).GetMethod(methodName);
+
+            return new FastMethodInvoker(mi);
+        }
+
+        sealed class Dummy
+        {
+            public void ActionNoArgs()
+            {
+            }
+
+            public void ActionOneArg(int i)
+            {
+            }
+
+            public int FuncNoArgs()
+            {
+                return 42;
+            }
+
+            public int FuncOneArg(int i)
+            {
+                return i;
+            }
+        }
+
+        public sealed class FastMethodInvoker
+        {
+            public MethodInfo MethodInfo { get; }
+            private readonly Func<object, object[], object> CompiledDelegate;
+
+            public FastMethodInvoker(MethodInfo methodInfo)
+            {
+                if (methodInfo == null) throw new ArgumentNullException(nameof(methodInfo));
+
+                MethodInfo = methodInfo;
+
+                ParameterExpression argumentsExpression = Expression.Parameter(typeof(object[]), "arguments");
+                ParameterInfo[] parameters = methodInfo.GetParameters();
+                Expression[] argumentExpressions = new Expression[parameters.Length];
+
+                for (int i = 0; i < parameters.Length; ++i)
+                {
+                    argumentExpressions[i] = Expression.Convert(
+                        Expression.ArrayIndex(argumentsExpression, Expression.Constant(i)),
+                        parameters[i].ParameterType
+                    );
+                }
+
+                ParameterExpression instanceExpression = null;
+                MethodCallExpression callExpression = null;
+
+                if (methodInfo.IsStatic)
+                {
+                    callExpression = Expression.Call(null, methodInfo, argumentExpressions);
+                }
+                else
+                {
+                    instanceExpression = Expression.Parameter(typeof(object), "instance");
+                    callExpression = Expression.Call(Expression.Convert(instanceExpression, methodInfo.ReflectedType), methodInfo, argumentExpressions);
+                }
+
+                if (methodInfo.ReturnType == typeof(void))
+                {
+                    Action<object, object[]> voidDelegate = Expression
+                        .Lambda<Action<object, object[]>>(callExpression, instanceExpression, argumentsExpression)
+                        .Compile();
+
+                    CompiledDelegate = (instance, arguments) =>
+                    {
+                        voidDelegate(instance, arguments);
+
+                        return null;
+                    };
+                }
+                else
+                {
+                    CompiledDelegate = Expression
+                        .Lambda<Func<object, object[], object>>(Expression.Convert(callExpression, typeof(object)), instanceExpression, argumentsExpression)
+                        .Compile();
+                }
+            }
+
+            public object Invoke(object instance, params object[] arguments)
+            {
+                return CompiledDelegate(instance, arguments);
+            }
+        }
+    }
+}
