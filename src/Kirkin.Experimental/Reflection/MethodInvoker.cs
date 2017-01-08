7ba662e8f3fafaaf2c483562206f099aef41a526
Kirill <kirill.shlenskiy@gmail.com>
2017-01-08 17:04:13 +1100

    FastMethodInvoker -> MethodInvoker, moved to Experimental

---
+++ src/Kirkin.Experimental/Reflection/MethodInvoker.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kirkin.Reflection
+{
+    /// <summary>
+    /// Provides functionality similar to <see cref="System.Reflection.MethodInfo"/> Invoke method.
+    /// Leverages compiled expression trees to improve on reflection's performance while maintaining a similar API.
+    /// </summary>
+    public sealed class MethodInvoker
+    {
+        private Func<object, object[], object> CompiledDelegate;
+
+        /// <summary>
+        /// Method invoked by this instance.
+        /// </summary>
+        public MethodInfo MethodInfo { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="MethodInvoker"/> instance.
+        /// </summary>
+        public MethodInvoker(MethodInfo methodInfo)
+        {
+            if (methodInfo == null) throw new ArgumentNullException(nameof(methodInfo));
+
+            MethodInfo = methodInfo;
+        }
+
+        /// <summary>
+        /// Invokes the target method on the given object instance (pass null if the method is static).
+        /// </summary>
+        public object Invoke(object instance, params object[] arguments)
+        {
+            if (CompiledDelegate == null) {
+                CompileDelegate();
+            }
+
+            return CompiledDelegate(instance, arguments);
+        }
+
+        // Expensive, so not calling it in constructor.
+        private void CompileDelegate()
+        {
+            ParameterExpression argumentsExpression = Expression.Parameter(typeof(object[]), "args");
+            ParameterInfo[] parameters = MethodInfo.GetParameters();
+            Expression[] argumentExpressions = new Expression[parameters.Length];
+
+            for (int i = 0; i < parameters.Length; ++i)
+            {
+                argumentExpressions[i] = Expression.Convert(
+                    Expression.ArrayIndex(argumentsExpression, Expression.Constant(i)),
+                    parameters[i].ParameterType
+                );
+            }
+
+            // Unfortunately needs to be allocated even for static method calls.
+            ParameterExpression instanceExpression = Expression.Parameter(typeof(object), "obj");
+
+            MethodCallExpression callExpression = MethodInfo.IsStatic
+                ? Expression.Call(null, MethodInfo, argumentExpressions)
+                : Expression.Call(Expression.Convert(instanceExpression, MethodInfo.ReflectedType), MethodInfo, argumentExpressions);
+
+            if (MethodInfo.ReturnType == typeof(void))
+            {
+                Action<object, object[]> voidDelegate = Expression
+                    .Lambda<Action<object, object[]>>(callExpression, instanceExpression, argumentsExpression)
+                    .Compile();
+
+                CompiledDelegate = (instance, arguments) =>
+                {
+                    voidDelegate(instance, arguments);
+
+                    return null;
+                };
+            }
+            else
+            {
+                CompiledDelegate = Expression
+                    .Lambda<Func<object, object[], object>>(Expression.Convert(callExpression, typeof(object)), instanceExpression, argumentsExpression)
+                    .Compile();
+            }
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Reflection/MethodInvokerTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Reflection;
+
+using Kirkin.Reflection;
+
+using Xunit;
+
+namespace Kirkin.Tests.Reflection
+{
+    public class MethodInvokerTests
+    {
+        [Fact]
+        public void MathMaxInvoke()
+        {
+            MethodInfo method = typeof(Math).GetMethod("Max", new[] { typeof(int), typeof(int) });
+            MethodInvoker invoker = new MethodInvoker(method);
+
+            Assert.Equal(2, invoker.Invoke(null, 1, 2));
+        }
+
+        [Fact]
+        public void TestActionNoArgs()
+        {
+            MethodInvoker invoker = CreateFastMethodInfo("ActionNoArgs");
+
+            Assert.Null(invoker.Invoke(new Dummy(), null));
+            Assert.Null(invoker.Invoke(new Dummy(), new int[0]));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, 42));
+        }
+
+        [Fact]
+        public void TestActionOneArg()
+        {
+            MethodInvoker invoker = CreateFastMethodInfo("ActionOneArg");
+
+            Assert.Null(invoker.Invoke(new Dummy(), 42));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, new object[0]));
+        }
+
+        [Fact]
+        public void TestFuncNoArgs()
+        {
+            MethodInvoker invoker = CreateFastMethodInfo("FuncNoArgs");
+
+            Assert.Equal(42, invoker.Invoke(new Dummy(), null));
+            Assert.Equal(42, invoker.Invoke(new Dummy(), new int[0]));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, 42));
+        }
+
+        [Fact]
+        public void TestFuncOneArg()
+        {
+            MethodInvoker invoker = CreateFastMethodInfo("FuncOneArg");
+
+            Assert.Equal(42, invoker.Invoke(new Dummy(), 42));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, null));
+            Assert.ThrowsAny<Exception>(() => invoker.Invoke(null, new object[0]));
+        }
+
+        private static MethodInvoker CreateFastMethodInfo(string methodName)
+        {
+            MethodInfo mi = typeof(Dummy).GetMethod(methodName);
+
+            return new MethodInvoker(mi);
+        }
+
+        sealed class Dummy
+        {
+            public void ActionNoArgs()
+            {
+            }
+
+            public void ActionOneArg(int i)
+            {
+            }
+
+            public int FuncNoArgs()
+            {
+                return 42;
+            }
+
+            public int FuncOneArg(int i)
+            {
+                return i;
+            }
+        }
+    }
+}
