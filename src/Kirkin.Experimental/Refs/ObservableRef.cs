4f04f45813d56aa214d6b2dcf2d1e1245c578016
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-01-10 17:12:59 +1100

    Experimental: ObservableRef<T> type

---
+++ src/Kirkin.Experimental/Refs/ObservableRef.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Kirkin.Refs
+{
+    internal sealed class ObservableRef<T> : Ref<T>
+    {
+        private T _value;
+
+        public ObservableRef()
+        {
+        }
+
+        public ObservableRef(T initialValue)
+        {
+            _value = initialValue;
+        }
+
+        public override T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                T oldValue = _value;
+                _value = value;
+
+                ValueSet?.Invoke(this, EventArgs.Empty);
+
+                if (!Equals(value, oldValue)) {
+                    ValueChanged?.Invoke(this, new ValueChangedEventArgs<T>(oldValue, value));
+                }
+            }
+        }
+
+        public event EventHandler<ValueChangedEventArgs<T>> ValueChanged;
+        public event EventHandler ValueSet;
+    }
+
+    public sealed class ValueChangedEventArgs<T> : EventArgs
+    {
+        public T OldValue { get; }
+        public T NewValue { get; }
+
+        internal ValueChangedEventArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
---
+++ src/Kirkin.Experimental/Refs/Ref.cs
@@ -0,0 +1,25 @@
+﻿namespace Kirkin.Refs
+{
+    /// <summary>
+    /// Reference to a value.
+    /// </summary>
+    internal abstract class Ref<T> : IRef
+    {
+        /// <summary>
+        /// Gets or sets the value that this reference is pointing to.
+        /// </summary>
+        public abstract T Value { get; set; }
+
+        object IRef.Value
+        {
+            get
+            {
+                return Value;
+            }
+            set
+            {
+                Value = (T)value;
+            }
+        }
+    }
+}
