1436466e7e4b34e8d297f7ab8455cb727429e89c
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-01-11 09:13:39 +1100

    ObservableRefTests

---
+++ src/Kirkin.Tests/Refs/ObservableRefTests.cs
@@ -0,0 +1,36 @@
+﻿using Kirkin.Refs;
+
+using Xunit;
+
+namespace Kirkin.Tests.Refs
+{
+    public class ObservableRefTests
+    {
+        [Fact]
+        public void ValueSetAndValueChanged()
+        {
+            ObservableRef<int> strongRef = new ObservableRef<int>(123);
+
+            int valueSetCount = 0;
+            int valueChangedCount = 0;
+
+            strongRef.ValueSet += (s, e) => valueSetCount++;
+            strongRef.ValueChanged += (s, e) => valueChangedCount++;
+
+            strongRef.Value = 321;
+
+            Assert.Equal(1, valueSetCount);
+            Assert.Equal(1, valueChangedCount);
+
+            strongRef.Value = 321;
+
+            Assert.Equal(2, valueSetCount);
+            Assert.Equal(1, valueChangedCount);
+
+            strongRef.Value++;
+
+            Assert.Equal(3, valueSetCount);
+            Assert.Equal(2, valueChangedCount);
+        }
+    }
+}
