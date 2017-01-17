cc4cddf086fc253d6b9e4763ef70bbb7b58ccf71
Kirill <kirill.shlenskiy@gmail.com>
2017-01-17 21:38:36 +1100

    IsolationContext tweaks

--- src/Kirkin/IsolationContext.cs
+++ src/Kirkin/IsolationContext.cs
@@ -4,9 +4,9 @@ using System.Reflection;
 namespace Kirkin
 {
     /// <summary>
-    /// Wraps and AppDomain and provides methods to execute code inside the domain.
-    /// Useful when the object needs to access shared (static) state which should
-    /// not leak to other similar objects.
+    /// Wraps an AppDomain and provides ways to execute code inside that AppDomain.
+    /// Useful when an object needs to access shared (static) state which needs to
+    /// be isolated from other, similar objects.
     /// </summary>
     public sealed class IsolationContext
         : IDisposable
@@ -24,7 +24,7 @@ namespace Kirkin
         }
 
         /// <summary>
-        /// Creates a new instance of the given type inside the isolated domain.
+        /// Creates a new instance of the given type inside the isolated AppDomain.
         /// Type T must have a parameterless constructor.
         /// </summary>
         public T CreateInstance<T>()
@@ -36,19 +36,21 @@ namespace Kirkin
         }
 
         /// <summary>
-        /// Creates a new instance of the given type inside the isolated domain.
-        /// Type T must have a constructor which matches the given arguments.
+        /// Creates a new instance of the given type inside the isolated AppDomain.
+        /// Type T must have a constructor which matches the provided arguments.
         /// </summary>
         public T CreateInstance<T>(params object[] args)
             where T : MarshalByRefObject
         {
+            if (args == null) throw new ArgumentNullException(nameof(args));
+
             ThrowIfDisposed();
 
             return (T)AppDomain.CreateInstanceAndUnwrap(typeof(T).Assembly.FullName, typeof(T).FullName, false, default(BindingFlags), null, args, null, null);
         }
 
         /// <summary>
-        /// Unloads the domain.
+        /// Unloads the underlying AppDomain.
         /// </summary>
         public void Dispose()
         {
