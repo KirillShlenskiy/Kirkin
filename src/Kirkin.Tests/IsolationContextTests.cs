1885ac55cfd359408f6a7fd7395eaac2e73c142b
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-01-17 20:19:35 +1100

    IsolationContextTest: failing

---
+++ src/Kirkin.Tests/IsolationContextTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xunit;
+
+namespace Kirkin.Tests
+{
+    public class IsolationContextTests
+    {
+        [Fact]
+        public void BasicIsolation()
+        {
+            StaticState.Value = "zzz";
+
+            using (IsolationContext context = new IsolationContext())
+            {
+                Manipulator manipulator = context.CreateInstance<Manipulator>();
+
+                Assert.Null(manipulator.Value);
+
+                manipulator.Value = "123";
+
+                Assert.Equal("123", manipulator.Value);
+                Assert.Equal("zzz", StaticState.Value);
+            }
+        }
+
+        sealed class Manipulator : MarshalByRefObject
+        {
+            public string Value
+            {
+                get
+                {
+                    return StaticState.Value;
+                }
+                set
+                {
+                    StaticState.Value = Value;
+                }
+            }
+        }
+
+        static class StaticState
+        {
+            public static string Value;
+        }
+    }
+}
