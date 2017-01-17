523489de7574973846a8a4309940b4bbe400db2c
Kirill <kirill.shlenskiy@gmail.com>
2017-01-17 21:24:09 +1100

    IsolationContext: work in progress

--- src/Kirkin/IsolationContext.cs
+++ src/Kirkin/IsolationContext.cs
@@ -30,9 +30,7 @@ namespace Kirkin
         public T CreateInstance<T>()
             where T : MarshalByRefObject
         {
-            if (AppDomain == null) {
-                throw new ObjectDisposedException(GetType().Name);
-            }
+            ThrowIfDisposed();
 
             return (T)AppDomain.CreateInstanceAndUnwrap(typeof(T).Assembly.FullName, typeof(T).FullName);
         }
@@ -41,12 +39,10 @@ namespace Kirkin
         /// Creates a new instance of the given type inside the isolated domain.
         /// Type T must have a constructor which matches the given arguments.
         /// </summary>
-        internal T CreateInstance<T>(params object[] args)
+        public T CreateInstance<T>(params object[] args)
             where T : MarshalByRefObject
         {
-            if (AppDomain == null) {
-                throw new ObjectDisposedException(GetType().Name);
-            }
+            ThrowIfDisposed();
 
             return (T)AppDomain.CreateInstanceAndUnwrap(typeof(T).Assembly.FullName, typeof(T).FullName, false, default(BindingFlags), null, args, null, null);
         }
@@ -61,6 +57,16 @@ namespace Kirkin
                 AppDomain.Unload(AppDomain);
 
                 AppDomain = null;
+            }
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ObjectDisposedException"/> if necessary.
+        /// </summary>
+        private void ThrowIfDisposed()
+        {
+            if (AppDomain == null) {
+                throw new ObjectDisposedException(GetType().Name);
             }
         }
     }
