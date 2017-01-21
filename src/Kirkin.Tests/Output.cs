64fcf4a828acd556f953b04fe7c729a6974d214e
Kirill <kirill.shlenskiy@gmail.com>
2017-01-21 22:14:49 +1100

    Output: comments

--- src/Kirkin.Tests/Output.cs
+++ src/Kirkin.Tests/Output.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+
 using Kirkin.Logging;
 
 namespace Kirkin.Tests
 {
+    /// <summary>
+    /// Output helper useful when converting xUnit projects to NUnit.
+    /// </summary>
     internal static class Output
     {
         private static readonly Logger Logger = new LoggerBuilder(WriteLine)
             .AddFormatter(EntryFormatter.TimestampNonEmptyEntries())
             .BuildLogger();
 
+        /// <summary>
+        /// Logs the given message using a <see cref="Kirkin.Logging.Logger"/>
+        /// instance which prefixes messages with timestamps.
+        /// </summary>
         public static void Log(string message)
         {
             Logger.Log(message);
         }
-
+        
+        /// <summary>
+        /// Writes the given message directly to the console.
+        /// </summary>
         public static void WriteLine(string message)
         {
             Console.WriteLine(message);
