2f6143a6671d227669ad00a6df5b0d74ab6015d3
Kirill <kirill.shlenskiy@gmail.com>
2017-01-25 22:23:42 +1100

    SqlServerDataImport: early work

---
+++ src/Kirkin.Experimental/Data/SqlClient/SqlServerDataImport.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Kirkin.Data.SqlClient
+{
+    public sealed class SqlServerDataImport
+    {
+        public string ConnectionString { get; }
+
+        public SqlServerDataImport(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public void CreateTableFromDataTable(string tableName, DataTable dataTable)
+        {
+            string sql = GetCreateTableSql(tableName, dataTable);
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static string GetCreateTableSql(string tableName, DataTable dataTable)
+        {
+            StringBuilder sql = new StringBuilder();
+
+            sql.AppendLine($"CREATE TABLE [{tableName}] (");
+
+            HashSet<DataColumn> keyColumns = new HashSet<DataColumn>(dataTable.PrimaryKey);
+
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                DataColumn column = dataTable.Columns[i];
+
+                sql.Append($"  [{column.ColumnName}] {SqlTypeFromColumn(column)}");
+
+                if (keyColumns.Contains(column)) {
+                    sql.Append(" PRIMARY KEY");
+                }
+
+                if (i == dataTable.Columns.Count - 1)
+                {
+                    sql.AppendLine();
+                }
+                else
+                {
+                    sql.AppendLine(",");
+                }
+            }
+
+            sql.AppendLine(");");
+
+            return sql.ToString();
+        }
+
+        private static string SqlTypeFromColumn(DataColumn column)
+        {
+            if (column.DataType == typeof(string))
+            {
+                // TODO: Better length resolution.
+                return "varchar(255) NULL";
+            }
+
+            string nullOrNotNull = column.AllowDBNull ? "NULL" : "NOT NULL";
+
+            if (column.DataType == typeof(int)) return $"int {nullOrNotNull}";
+            if (column.DataType == typeof(decimal)) return $"money {nullOrNotNull}";
+
+            throw new NotSupportedException($"Unsupported data type for column '{column.ColumnName}'.");
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Data/SqlClient/SqlServerDataImportTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+using Kirkin.Data.SqlClient;
+
+using NUnit.Framework;
+
+namespace Kirkin.Tests.Data.SqlClient
+{
+    public sealed class SqlServerDataImportTests
+    {
+        private static readonly string ConnectionString = new SqlConnectionStringBuilder {
+            DataSource = @"KIRKINPUTER\SQL2008R2",
+            InitialCatalog = "Test",
+            IntegratedSecurity = true
+        }.ToString();
+
+        public SqlServerDataImportTests()
+        {
+            if (!Environment.MachineName.Equals("KIRKINPUTER", StringComparison.OrdinalIgnoreCase)) {
+                Assert.Ignore("This test only runs on KIRKINPUTER.");
+            }
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Value", typeof(decimal));
+
+            dt.PrimaryKey = new[] {
+                dt.Columns["ID"]
+            };
+
+            dt.Rows.Add(1, "Aaa", 123.45m);
+            dt.Rows.Add(2, "Bbb", 321.00);
+
+            SqlServerDataImport import = new SqlServerDataImport(ConnectionString);
+
+            import.CreateTableFromDataTable("TestTable", dt);
+        }
+    }
+}
