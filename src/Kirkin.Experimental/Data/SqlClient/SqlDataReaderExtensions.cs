e2a0b7e80612033e36a8ddefa18133c7edc274dd
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-02-07 18:12:57 +1100

    SqlDataReaderExtensions refactoring

--- src/Kirkin.Experimental/Data/SqlClient/SqlDataReaderExtensions.cs
+++ src/Kirkin.Experimental/Data/SqlClient/SqlDataReaderExtensions.cs
@@ -22,38 +22,47 @@ namespace Kirkin.Data.SqlClient
         /// </summary>
         public static T GetValueOrDefaultAlt<T>(this SqlDataReader reader, int index)
         {
-            Type fieldType = reader.GetFieldType(index);
+            if (reader.IsDBNull(index)) {
+                return default(T);
+            }
+
+            Type fieldType = typeof(T);
 
             if (fieldType.IsValueType)
             {
-                // Try get well-known type.
-                if (reader.IsDBNull(index)) {
-                    return default(T);
-                }
+                // Try resolve well-known delegate for this return type.
+                object func = WellKnownGetValueDelegates.Find(fieldType);
 
-                object delegateObj = GetValueOrDefaultDelegate(fieldType);
-
-                if (delegateObj != null) {
-                    return ((Func<SqlDataReader, int, T>)delegateObj).Invoke(reader, index);
+                if (func != null) {
+                    return ((Func<SqlDataReader, int, T>)func).Invoke(reader, index);
                 }
             }
 
-            return DefaultIfDbNull<T>(reader.GetValue(index));
+            return (T)reader.GetValue(index);
         }
 
-        private static T DefaultIfDbNull<T>(object value)
+        static class WellKnownGetValueDelegates
         {
-            return value is DBNull ? default(T) : (T)value;
-        }
+            private static readonly Func<SqlDataReader, int, bool> Boolean = (r, i) => r.GetBoolean(i);
+            private static readonly Func<SqlDataReader, int, DateTime> DateTime = (r, i) => r.GetDateTime(i);
+            private static readonly Func<SqlDataReader, int, decimal> Decimal = (r, i) => r.GetDecimal(i);
+            private static readonly Func<SqlDataReader, int, double> Double = (r, i) => r.GetDouble(i);
+            private static readonly Func<SqlDataReader, int, float> Float = (r, i) => r.GetFloat(i);
+            private static readonly Func<SqlDataReader, int, int> Int32 = (r, i) => r.GetInt32(i);
+            private static readonly Func<SqlDataReader, int, long> Int64 = (r, i) => r.GetInt64(i);
 
-        private static object GetValueOrDefaultDelegate(Type fieldType)
-        {
-            if (fieldType == typeof(int)) return new Func<SqlDataReader, int, int>((r, i) => r.GetInt32(i));
-            if (fieldType == typeof(bool)) return new Func<SqlDataReader, int, bool>((r, i) => r.GetBoolean(i));
-            if (fieldType == typeof(decimal)) return new Func<SqlDataReader, int, decimal>((r, i) => r.GetDecimal(i));
-            if (fieldType == typeof(long)) return new Func<SqlDataReader, int, long>((r, i) => r.GetInt64(i));
+            public static object Find(Type fieldType)
+            {
+                if (fieldType == typeof(int)) return Int32;
+                if (fieldType == typeof(bool)) return Boolean;
+                if (fieldType == typeof(decimal)) return Decimal;
+                if (fieldType == typeof(DateTime)) return DateTime;
+                if (fieldType == typeof(long)) return Int64;
+                if (fieldType == typeof(double)) return Double;
+                if (fieldType == typeof(float)) return Float;
 
-            return null;
+                return null;
+            }
         }
     }
 }
