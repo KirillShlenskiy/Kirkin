c8af5ce5e17acd073e8b83b760d158c6c6614fe0
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-02-07 16:25:11 +1100

    Experimental: SqlDataReaderExtensions

---
+++ src/Kirkin.Experimental/Data/SqlClient/SqlDataReaderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Kirkin.Data.SqlClient
+{
+    public static class SqlDataReaderExtensions
+    {
+        // Key = field type.
+        // Value = resolve value at index delegate (Func<SqlDataReader, int, T>).
+        private static readonly Dictionary<Type, object> Delegates = new Dictionary<Type, object> {
+            { typeof(bool), new Func<SqlDataReader, int, bool>((r, i) => r.GetBoolean(i)) },
+            { typeof(int), new Func<SqlDataReader, int, int>((r, i) => r.GetInt32(i)) },
+            { typeof(long), new Func<SqlDataReader, int, long>((r, i) => r.GetInt64(i)) },
+            { typeof(decimal), new Func<SqlDataReader, int, decimal>((r, i) => r.GetDecimal(i)) },
+        };
+
+        /// <summary>
+        /// Gets the value of the field at the specified index with
+        /// minimal allocations at the expense of some CPU cycles.
+        /// </summary>
+        public static T GetValueOrDefaultAlt<T>(this SqlDataReader reader, string name)
+        {
+            int index = reader.GetOrdinal(name);
+
+            return reader.GetValueOrDefaultAlt<T>(index);
+        }
+
+        /// <summary>
+        /// Gets the value of the field at the specified index with
+        /// minimal allocations at the expense of some CPU cycles.
+        /// </summary>
+        public static T GetValueOrDefaultAlt<T>(this SqlDataReader reader, int index)
+        {
+            if (reader.IsDBNull(index)) {
+                return default(T);
+            }
+
+            Type fieldType = reader.GetFieldType(index);
+            object delegateObj;
+
+            // Lookup + cast may seem slow but it's better than boxing / GC pressure.
+            if (Delegates.TryGetValue(fieldType, out delegateObj)) {
+                return ((Func<SqlDataReader, int, T>)delegateObj).Invoke(reader, index);
+            }
+
+            return (T)reader[index];
+        }
+    }
+}
---
+++ src/Kirkin.Tests/Data/SqlClient/SqlDataReaderExtensionsTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data.SqlClient;
+using Kirkin.Data.SqlClient;
+
+using NUnit.Framework;
+
+namespace Kirkin.Tests.Data.SqlClient
+{
+    public class SqlDataReaderExtensionsTests
+    {
+        private const string ConnectionString = "Data Source=MENZIES; Initial Catalog=master; Integrated Security=True;";
+        private const string SQL = "SELECT ROW_NUMBER() OVER ( ORDER BY id ) FROM sysobjects";
+        private const int Iterations = 1000;
+
+        [Test]
+        public static void Regular()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < Iterations; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(SQL, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        long result;
+
+                        while (reader.Read()) {
+                            result = (long)reader[0];
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Collection counts:");
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Gen {i}: {GC.CollectionCount(i)}.");
+            }
+        }
+
+        [Test]
+        public static void Frugal()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < Iterations; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(SQL, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        long result;
+
+                        while (reader.Read()) {
+                            result = reader.GetValueOrDefaultAlt<long>(0);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Gen {i}: {GC.CollectionCount(i)}.");
+            }
+        }
+
+        public static class MemoryAnalysis
+        {
+            public static long AveragedApproximateSize<T>(Func<T> rootGenerator) where T : class
+            {
+                const int iters = 1000000;
+
+                var items = new object[iters];
+                long start = GC.GetTotalMemory(true);
+
+                for (int i = 0; i < items.Length; i++)
+                    items[i] = rootGenerator();
+
+                long end = GC.GetTotalMemory(true);
+                GC.KeepAlive(items);
+
+                return (long)Math.Round((end - start) / (double)iters);
+            }
+        }
+    }
+}
