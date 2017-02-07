d970af589be898e3b79728222735356cb85c07a1
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-02-07 18:29:10 +1100

    SqlDataReaderExtensionTests tweaks

--- src/Kirkin.Tests/Data/SqlClient/SqlDataReaderExtensionsTests.cs
+++ src/Kirkin.Tests/Data/SqlClient/SqlDataReaderExtensionsTests.cs
@@ -10,8 +10,16 @@ namespace Kirkin.Tests.Data.SqlClient
     public class SqlDataReaderExtensionsTests
     {
         private const string ConnectionString = "Data Source=.; Initial Catalog=master; Integrated Security=True;";
-        private const string SQL = "SELECT ROW_NUMBER() OVER ( ORDER BY id ) FROM sysobjects";
-        private const int Iterations = 2000;
+        private const string SqlRowNum = "SELECT ROW_NUMBER() OVER ( ORDER BY id ) FROM sysobjects";
+        private const string SqlNull = "SELECT NULL FROM sysobjects";
+        private const int Iterations = 1000;
+
+        public SqlDataReaderExtensionsTests()
+        {
+            if (!Environment.MachineName.Equals("BABUSHKA", StringComparison.OrdinalIgnoreCase)) {
+                Assert.Ignore("");
+            }
+        }
 
         [Test]
         public static void Regular()
@@ -24,7 +32,7 @@ namespace Kirkin.Tests.Data.SqlClient
 
                 for (int i = 0; i < Iterations; i++)
                 {
-                    using (SqlCommand command = new SqlCommand(SQL, connection))
+                    using (SqlCommand command = new SqlCommand(SqlRowNum, connection))
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         long result;
@@ -54,13 +62,103 @@ namespace Kirkin.Tests.Data.SqlClient
 
                 for (int i = 0; i < Iterations; i++)
                 {
-                    using (SqlCommand command = new SqlCommand(SQL, connection))
+                    using (SqlCommand command = new SqlCommand(SqlRowNum, connection))
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         long result;
 
                         while (reader.Read()) {
                             result = reader.GetValueOrDefault<long>(0);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Collection counts:");
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Gen {i}: {counter.CollectionCount(i)}.");
+            }
+        }
+
+        [Test]
+        public static void DbNullBenchmarkRegular()
+        {
+            GCCollectionCounter counter = GCCollectionCounter.StartNew();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < Iterations; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(SqlRowNum, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        bool result;
+
+                        while (reader.Read()) {
+                            result = reader[0] is DBNull;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Collection counts:");
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Gen {i}: {counter.CollectionCount(i)}.");
+            }
+        }
+
+        [Test]
+        public static void DbNullBenchmarkIsNull()
+        {
+            GCCollectionCounter counter = GCCollectionCounter.StartNew();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < Iterations; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(SqlRowNum, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        bool result;
+
+                        while (reader.Read()) {
+                            result = reader.IsDBNull(0);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Collection counts:");
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Gen {i}: {counter.CollectionCount(i)}.");
+            }
+        }
+
+        [Test]
+        public static void DbNullBenchmarkOptimized()
+        {
+            GCCollectionCounter counter = GCCollectionCounter.StartNew();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < Iterations; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(SqlRowNum, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        bool result;
+
+                        while (reader.Read()) {
+                            result = reader.GetValueOrDefault<long>(0) == 0;
                         }
                     }
                 }
