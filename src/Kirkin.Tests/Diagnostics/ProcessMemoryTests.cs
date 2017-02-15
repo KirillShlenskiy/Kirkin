dbc04ed1a2c3f78fef0530994599ba95273614d4
Kirill <kirill.shlenskiy@gmail.com>
2017-02-15 21:13:54 +1100

    ProcessMemoryTests fixed

--- src/Kirkin.Tests/Diagnostics/ProcessMemoryTests.cs
+++ src/Kirkin.Tests/Diagnostics/ProcessMemoryTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Kirkin.Diagnostics;
 
@@ -15,28 +10,29 @@ namespace Kirkin.Tests.Diagnostics
     public class ProcessMemoryTests
     {
         [Test]
-        public void ReadWriteNotepadMemory()
+        public void ReadNotepadMemory()
         {
             Directory.CreateDirectory(@"C:\Temp");
             string filePath = @"C:\Temp\Text.txt";
 
-            File.WriteAllText(filePath, "Hello world.", Encoding.Unicode);
+            File.WriteAllText(filePath, "Hello world.");
 
-            using (Process notepad = Process.Start("notepad", $"/W {filePath}"))
+            byte[] bytes;
+
+            using (Process notepad = Process.Start("notepad", filePath))
             {
                 try
                 {
                     ProcessMemory memory = new ProcessMemory(notepad);
-                    byte[] allMemory = memory.Read();
-                    string allText = Encoding.UTF8.GetString(allMemory);
-
-                    Assert.True(allText.IndexOf("Hello world", StringComparison.OrdinalIgnoreCase) != -1, "Text not found.");
+                    bytes = memory.Read();
                 }
                 finally
                 {
                     notepad.Kill();
                 }
             }
+
+            Assert.AreNotEqual(0, bytes.Length);
         }
     }
 }
