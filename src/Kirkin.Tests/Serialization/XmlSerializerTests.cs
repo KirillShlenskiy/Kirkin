513d4d5bd2ebefd40f717fe6b1206fb25f02d41e
Kirill <kirill.shlenskiy@gmail.com>
2017-05-03 22:28:18 +1000

    Basic XmlSerializerTests

---
+++ src/Kirkin.Tests/Serialization/XmlSerializerTests.cs
@@ -0,0 +1,36 @@
+﻿using Kirkin.ChangeTracking;
+using Kirkin.Serialization;
+
+using NUnit.Framework;
+
+namespace Kirkin.Tests.Serialization
+{
+    public class XmlSerializerTests
+    {
+        [Test]
+        public void BasicSerialization()
+        {
+            XmlSerializer serializer = new XmlSerializer();
+            Dummy original = new Dummy { ID = 123, Value = "Test" };
+            string serialized = serializer.Serialize(original);
+
+            Assert.AreEqual(
+                @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Root>
+  <ID>123</ID>
+  <Value>Test</Value>
+</Root>", serialized);
+
+            Dummy clone = serializer.Deserialize<Dummy>(serialized);
+
+            Assert.True(PropertyValueEqualityComparer<Dummy>.Default.Equals(original, clone));
+            Assert.AreNotSame(original, clone);
+        }
+
+        public class Dummy
+        {
+            public int ID { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
