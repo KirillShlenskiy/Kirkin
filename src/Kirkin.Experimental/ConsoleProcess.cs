4cf15d6c951eaed0e76c1a89f4601a68d64220fa
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-05-03 13:21:07 +1000

    ConsoleProcess: early work

---
+++ src/Kirkin.Experimental/ConsoleProcess.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kirkin
+{
+    /// <summary>
+    /// <see cref="Process"/> wrapper that facilitates console app interop.
+    /// </summary>
+    public sealed class ConsoleProcess
+    {
+        /// <summary>
+        /// Executable name/path specified when this instance was created.
+        /// </summary>
+        public string FileName { get; }
+
+        /// <summary>
+        /// Arguments specified when this instance was created.
+        /// </summary>
+        public string Arguments { get; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ConsoleProcess"/> with the given executable name and args.
+        /// </summary>
+        private ConsoleProcess(string fileName, string args = null)
+        {
+            if (string.IsNullOrEmpty(fileName)) throw new ArgumentException("File name must be specified.");
+
+            FileName = fileName;
+            Arguments = args;
+        }
+
+        /// <summary>
+        /// Raised when output is received from the console app.
+        /// Gets raised on a background thread.
+        /// </summary>
+        public event Action<string> Output;
+
+        /// <summary>
+        /// Runs the process.
+        /// </summary>
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(FileName) {
+                Arguments = Arguments,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            Process process = null;
+
+            EventHandler processExitHandler = delegate
+            {
+                Process p = Interlocked.Exchange(ref process, null);
+
+                if (p != null) {
+                    p.Kill();
+                };
+            };
+
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+
+            cancellationToken.Register(() => processExitHandler(null, null), useSynchronizationContext: false);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            AppDomain.CurrentDomain.ProcessExit += processExitHandler;
+
+            try
+            {
+                process = Process.Start(processStartInfo);
+
+                process.EnableRaisingEvents = true;
+
+                process.OutputDataReceived += (s, e) =>
+                {
+                    string line = e.Data;
+
+                    Output?.Invoke(line);
+                };
+
+                process.BeginOutputReadLine();
+
+                process.Exited += (s, e) => tcs.SetResult(null);
+
+                await tcs.Task.ConfigureAwait(false); // Long-running operation.
+
+                // By now replmergProcess could have been killed and nulled out by the
+                // OutputDataReceived event handler. Handle this scenario gracefully.
+                Process p = process;
+
+                //Thread.MemoryBarrier(); // Barrier inserted by the await.
+
+                if (p == null)
+                {
+                    throw new OperationCanceledException("Child process forcibly terminated.", cancellationToken);
+                }
+                else
+                {
+                    int result = p.ExitCode;
+
+                    if (result != 0) {
+                        throw new Win32Exception(result, "Non-zero exit code.");
+                    }
+                }
+            }
+            finally
+            {
+                AppDomain.CurrentDomain.ProcessExit -= processExitHandler;
+                Process p = Interlocked.Exchange(ref process, null);
+
+                if (p != null) {
+                    p.Close();
+                }
+            }
+        }
+    }
+}
