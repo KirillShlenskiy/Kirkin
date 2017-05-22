45bfa6912a3808f2bd0f26d3851d66eeb8d2e311
Kirill <kirill.shlenskiy@gmail.com>
2017-05-22 21:42:07 +1000

    Version change (v0.9.4), msbuild 15

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Rebuild /verbosity:minimal || exit /b
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\src\Kirkin.sln /p:Configuration=Release /p:Platform="Any CPU" /target:Rebuild /verbosity:minimal || exit /b
 nuget pack Kirkin.nuspec -IncludeReferencedProjects -Properties Configuration=Release || exit /b
 nuget pack Kirkin.Sources.nuspec -IncludeReferencedProjects
