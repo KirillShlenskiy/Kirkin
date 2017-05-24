349cf43bf45e88b005a18fa88affc50dc5457764
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-05-24 18:25:06 +1000

    CommandLine: test CLI console project

---
+++ src/cli/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Kirkin.CommandLine;
+
+namespace cli
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                CommandLineParser parser = new CommandLineParser();
+
+                parser.DefineCommand("hello", hello =>
+                {
+                    CommandArg<string> name = hello.DefineOption("name", shortName: "n");
+
+                    hello.Executed += () => Console.WriteLine($"Hello {name.Value}!");
+                });
+
+                ICommand command = parser.Parse(args);
+
+                command.Execute();
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor color = Console.ForegroundColor;
+
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ForegroundColor = color;
+                }
+
+                return ex.HResult;
+            }
+
+            return 0;
+        }
+    }
+}
