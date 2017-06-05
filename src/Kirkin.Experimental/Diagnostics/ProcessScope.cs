c57d77e1cb637322500a6c51c8c92d3e21525eee
Kirill <kirill.shlenskiy@gmail.com>
2017-06-06 09:35:44 +1000

    ProcessScope: early work

---
+++ src/Kirkin.Experimental/Diagnostics/ProcessScope.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirkin.Diagnostics
+{
+    public sealed class ProcessScope : IDisposable
+    {
+        private readonly EventHandler CurrentDomainExitHandler;
+        private Process _process;
+
+        public Process Process
+        {
+            get
+            {
+                return _process;
+            }
+        }
+
+        public ProcessScope(Process process)
+        {
+            _process = process;
+
+            CurrentDomainExitHandler = delegate {
+                KillChildProcess();
+            };
+
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainExitHandler;
+        }
+
+        private void KillChildProcess()
+        {
+            if (!Process.HasExited)
+            {
+                Process.Kill();
+            }
+        }
+
+        public void Dispose()
+        {
+            AppDomain.CurrentDomain.ProcessExit -= CurrentDomainExitHandler;
+        }
+    }
+}
