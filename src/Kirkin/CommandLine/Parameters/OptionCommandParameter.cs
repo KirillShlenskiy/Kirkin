0e2587b57aad8eb705ebe9e4828d09069fe16cbb
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-06-08 19:33:26 +1000

    OptionCommandParameter: return empty string if option value unspecified

--- src/Kirkin/CommandLine/Parameters/OptionCommandParameter.cs
+++ src/Kirkin/CommandLine/Parameters/OptionCommandParameter.cs
@@ -21,7 +21,9 @@ namespace Kirkin.CommandLine.Parameters
 
         public override string ParseArgs(List<string> args)
         {
-            if (args.Count == 0) return null;
+            // If the option is specified, we want a value
+            // different from the default (which is null).
+            if (args.Count == 0) return string.Empty;
             if (args.Count > 1) throw new InvalidOperationException($"Multiple argument values are not supported for option '{Name}'.");
 
             return args[0];
