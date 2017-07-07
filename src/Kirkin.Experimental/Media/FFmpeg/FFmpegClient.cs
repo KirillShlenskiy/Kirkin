36c124df64685ed898654bb64ade81af54502a12
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-07-07 17:48:30 +1000

    FFmpegClient: work in progress

--- src/Kirkin.Experimental/Media/FFmpeg/FFmpegClient.cs
+++ src/Kirkin.Experimental/Media/FFmpeg/FFmpegClient.cs
@@ -19,14 +19,14 @@ namespace Kirkin.Media.FFmpeg
         public string FFmpegPath { get; }
 
         /// <summary>
-        /// Target audio bitrate in KB/sec. The default is 192.
+        /// Target audio bitrate in Kb/sec. The default is 192.
         /// </summary>
         public int AudioBitrate { get; set; } = 192;
 
         /// <summary>
-        /// Number of audio channels. The default is 2 (stereo).
+        /// Number of audio channels. The default is zero (original).
         /// </summary>
-        public int AudioChannels { get; set; } = 2;
+        public int AudioChannels { get; set; } = 0;
 
         /// <summary>
         /// Audio encoder. The default is "aac".
@@ -34,7 +34,7 @@ namespace Kirkin.Media.FFmpeg
         public string AudioEncoder { get; set; } = "aac";
 
         /// <summary>
-        /// Target video bitrate in KB/sec. The default is 2048.
+        /// Target video bitrate in Kb/sec. The default is 2048.
         /// </summary>
         public int VideoBitrate { get; set; } = 2048;
 
@@ -42,6 +42,11 @@ namespace Kirkin.Media.FFmpeg
         /// Video encoder. The default is "libx264".
         /// </summary>
         public string VideoEncoder { get; set; } = "libx264";
+
+        /// <summary>
+        /// Video height (480p, 720p, 1080p). The default is zero (original).
+        /// </summary>
+        public int VideoHeight { get; set; } = 0;
 
         /// <summary>
         /// Creates a new ffmpeg wrapper instance without specifying the exact ffmpeg.exe path.
@@ -72,8 +77,14 @@ namespace Kirkin.Media.FFmpeg
             if (VideoEncoder != null)
                 args.Add("-c:v " + VideoEncoder);
 
+            if (string.Equals(VideoEncoder, "libx264", StringComparison.OrdinalIgnoreCase))
+                args.Add("-profile:v high -preset slow");
+
             if (VideoBitrate != 0)
-                args.Add("-b:v " + VideoBitrate + "k");
+                args.Add($"-b:v {VideoBitrate}k -maxrate {VideoBitrate}k -bufsize {VideoBitrate * 2}k");
+
+            if (VideoHeight != 0)
+                args.Add("-vf scale=-2:" + VideoHeight);
 
             if (AudioEncoder != null)
                 args.Add("-c:a " + AudioEncoder);
