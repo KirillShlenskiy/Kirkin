00125cbabd45779a07f8f623b247193da9b447e1
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-07-18 18:05:00 +1000

    ClrMdTests: work in progress

--- src/Kirkin.Tests/Experimental/ClrMdTests.cs
+++ src/Kirkin.Tests/Experimental/ClrMdTests.cs
@@ -21,11 +21,11 @@ namespace Kirkin.Tests.Experimental
         [Test]
         public void BasicDump()
         {
-            using (var dataTarget = DataTarget.AttachToProcess(PID, 10000, AttachFlag.Invasive))
+            using (DataTarget dataTarget = DataTarget.AttachToProcess(PID, 10000, AttachFlag.Invasive))
             {
                 // Dump CLR info
-                var clrVersion = dataTarget.ClrVersions.First();
-                var dacInfo = clrVersion.DacInfo;
+                ClrInfo clrVersion = dataTarget.ClrVersions[0];
+                DacInfo dacInfo = clrVersion.DacInfo;
 
                 Console.WriteLine("# CLR Info");
                 Console.WriteLine("Version:   {0}", clrVersion.Version);
@@ -36,23 +36,59 @@ namespace Kirkin.Tests.Experimental
         }
 
         [Test]
+        public void TypeStats()
+        {
+            using (DataTarget dataTarget = DataTarget.AttachToProcess(PID, 10000, AttachFlag.Invasive))
+            {
+                ClrInfo clrVersion = dataTarget.ClrVersions[0];
+                ClrRuntime runtime = clrVersion.CreateRuntime();
+                Dictionary<string, int> countsByClrType = new Dictionary<string, int>();
+
+                foreach (ulong ptr in runtime.Heap.EnumerateObjectAddresses())
+                {
+                    ClrType type = runtime.Heap.GetObjectType(ptr);
+                    int count;
+
+                    if (countsByClrType.TryGetValue(type.Name, out count))
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+
+                    countsByClrType[type.Name] = count;
+                }
+
+                foreach (KeyValuePair<string, int> kvp in countsByClrType.OrderByDescending(v => v.Value))
+                {
+                    if (kvp.Value < 10) {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }
+            }
+        }
+
+        [Test]
         public void StringDump()
         {
-            using (var dataTarget = DataTarget.AttachToProcess(PID, 10000, AttachFlag.Invasive))
+            using (DataTarget dataTarget = DataTarget.AttachToProcess(PID, 10000, AttachFlag.Invasive))
             {
-                var clrVersion = dataTarget.ClrVersions.First();
-                var runtime = clrVersion.CreateRuntime();
-                var heap = runtime.Heap;
-                var numberOfStrings = 0;
-                var uniqueStrings = new Dictionary<string, int>();
+                ClrInfo clrVersion = dataTarget.ClrVersions[0];
+                ClrRuntime runtime = clrVersion.CreateRuntime();
+                ClrHeap heap = runtime.Heap;
+                int numberOfStrings = 0;
+                Dictionary<string, int> uniqueStrings = new Dictionary<string, int>();
 
-                foreach (var ptr in heap.EnumerateObjectAddresses())
+                foreach (ulong ptr in heap.EnumerateObjectAddresses())
                 {
-                    var type = heap.GetObjectType(ptr);
+                    ClrType type = heap.GetObjectType(ptr);
 
                     // Skip if not a string
-                    if (type == null || type.IsString == false)
-                    {
+                    if (type == null || type.IsString == false) {
                         continue;
                     }
 
@@ -60,7 +96,7 @@ namespace Kirkin.Tests.Experimental
                     numberOfStrings++;
 
                     // Get value
-                    var text = (string)type.GetValue(ptr);
+                    string text = (string)type.GetValue(ptr);
 
                     if (uniqueStrings.ContainsKey(text))
                     {
@@ -75,9 +111,9 @@ namespace Kirkin.Tests.Experimental
                 Console.WriteLine("## String info");
                 Console.WriteLine("String count:     {0}", numberOfStrings);
                 Console.WriteLine("");
-
                 Console.WriteLine("Most duplicated strings: (top 5)");
-                foreach (var keyValuePair in uniqueStrings.OrderByDescending(kvp => kvp.Value).Take(5))
+
+                foreach (KeyValuePair<string, int> keyValuePair in uniqueStrings.OrderByDescending(kvp => kvp.Value).Take(5))
                 {
                     Console.WriteLine("* {0} usages: {1}", keyValuePair.Value, keyValuePair.Key);
                 }
