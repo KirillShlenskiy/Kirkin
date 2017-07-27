c79fbca40e786c4dbc27a748bf78debce02a0469
Kirill <kirill.shlenskiy@gmail.com>
2017-07-28 09:01:16 +1000

    Experimental: basic AsyncLock

---
+++ src/Kirkin.Experimental/Threading/AsyncLock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kirkin.Threading
+{
+    public sealed class AsyncLock
+    {
+        private TaskCompletionSource<bool> _tcs;
+
+        public async Task<AsyncLockReleaser> EnterAsync()
+        {
+            TaskCompletionSource<bool> newTcs = new TaskCompletionSource<bool>();
+            TaskCompletionSource<bool> oldTcs = Interlocked.Exchange(ref _tcs, newTcs);
+
+            if (oldTcs != null)
+            {
+                // TODO: async completion?
+                await oldTcs.Task.ConfigureAwait(false);
+            }
+
+            return new AsyncLockReleaser(newTcs);
+        }
+    }
+
+    public struct AsyncLockReleaser : IDisposable
+    {
+        private readonly TaskCompletionSource<bool> _tcs;
+
+        public AsyncLockReleaser(TaskCompletionSource<bool> tcs)
+        {
+            _tcs = tcs;
+        }
+
+        public void Dispose()
+        {
+            _tcs.SetResult(true);
+        }
+    }
+}
