a1809489c7fc7bc0213b211027250788398291e2
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2017-07-28 15:36:01 +1000

    AsyncLock tweaks

--- src/Kirkin.Experimental/Threading/AsyncLock.cs
+++ src/Kirkin.Experimental/Threading/AsyncLock.cs
@@ -4,18 +4,23 @@ using System.Threading.Tasks;
 
 namespace Kirkin.Threading
 {
+    /// <summary>
+    /// Fast non-cancelable async lock.
+    /// </summary>
     public sealed class AsyncLock
     {
         private TaskCompletionSource<bool> _tcs;
 
+        /// <summary>
+        /// Enters the lock asynchronously and returns an <see cref="IDisposable"/>
+        /// object which releases the lock when disposed.
+        /// </summary>
         public async Task<AsyncLockReleaser> EnterAsync()
         {
             TaskCompletionSource<bool> newTcs = new TaskCompletionSource<bool>();
             TaskCompletionSource<bool> oldTcs = Interlocked.Exchange(ref _tcs, newTcs);
 
-            if (oldTcs != null)
-            {
-                // TODO: async completion?
+            if (oldTcs != null) {
                 await oldTcs.Task.ConfigureAwait(false);
             }
 
@@ -23,7 +28,10 @@ namespace Kirkin.Threading
         }
     }
 
-    public struct AsyncLockReleaser : IDisposable
+    /// <summary>
+    /// <see cref="AsyncLock"/> releaser structure.
+    /// </summary>
+    public sealed class AsyncLockReleaser : IDisposable
     {
         private readonly TaskCompletionSource<bool> _tcs;
 
@@ -32,6 +40,9 @@ namespace Kirkin.Threading
             _tcs = tcs;
         }
 
+        /// <summary>
+        /// Releases the <see cref="AsyncLock"/>.
+        /// </summary>
         public void Dispose()
         {
             _tcs.TrySetResult(true);
