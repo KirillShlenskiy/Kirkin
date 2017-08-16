abb6d67a4a364f9ea6c482292e6f5b2878bf131a
Kirill <kirill.shlenskiy@gmail.com>
2017-08-17 01:06:43 +1000

    Experimental: SqlCommandExtensions2

---
+++ src/Kirkin.Experimental/Data/SqlClient/SqlCommandExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Kirkin.Data.SqlClient
+{
+    public static partial class SqlCommandExtensions2
+    {
+        public static DataTableLite ExecuteDataTableLite(this SqlCommand command)
+        {
+            if (command.Connection.State != ConnectionState.Open) {
+                command.Connection.Open();
+            }
+
+            using (SqlDataReader reader = command.ExecuteReader()) {
+                return TableFromReader(reader);
+            }
+        }
+
+        private static DataTableLite TableFromReader(SqlDataReader reader)
+        {
+            DataTableLite table = new DataTableLite();
+
+            for (int i = 0; i < reader.FieldCount; i++) {
+                table.Columns.Add(reader.GetName(i), reader.GetFieldType(i));
+            }
+
+            while (reader.Read())
+            {
+                object[] itemArray = new object[reader.FieldCount];
+
+                for (int i = 0; i < itemArray.Length; i++) {
+                    itemArray[i] = reader[i];
+                }
+
+                table.Rows.Add(itemArray);
+            }
+
+            table.Rows.TrimExcess(); // Manage GC pressure.
+
+            return table;
+        }
+    }
+}
