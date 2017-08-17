d8d5ce18b77d7380c51ea0ef680546e8d3cde10b
Kirill <kirill.shlenskiy@gmail.com>
2017-08-17 23:10:55 +1000

    DataTableLiteTests: more tests

--- src/Kirkin.Tests/Data/DataTableLiteTests.cs
+++ src/Kirkin.Tests/Data/DataTableLiteTests.cs
@@ -1,4 +1,6 @@
-﻿using Kirkin.Data;
+﻿using System;
+
+using Kirkin.Data;
 
 using NUnit.Framework;
 
@@ -16,12 +18,24 @@ namespace Kirkin.Tests.Data
 
             dt.Rows.Add(1, "Blah");
             dt.Rows.Add(2, "Zzz");
+            dt.Rows.Add(3, DBNull.Value);
 
-            Assert.AreEqual(2, dt.Rows.Count);
+            Assert.AreEqual(3, dt.Rows.Count);
+
             Assert.AreEqual(1, dt.Rows[0][0]);
             Assert.AreEqual("Blah", dt.Rows[0][1]);
+            Assert.AreEqual(1, dt.Rows[0]["id"]);
+            Assert.AreEqual("Blah", dt.Rows[0]["value"]);
+
             Assert.AreEqual(2, dt.Rows[1][0]);
             Assert.AreEqual("Zzz", dt.Rows[1][1]);
+            Assert.AreEqual(2, dt.Rows[1]["id"]);
+            Assert.AreEqual("Zzz", dt.Rows[1]["value"]);
+
+            Assert.AreEqual(3, dt.Rows[2][0]);
+            Assert.AreEqual(DBNull.Value, dt.Rows[2][1]);
+            Assert.AreEqual(3, dt.Rows[2]["id"]);
+            Assert.AreEqual(DBNull.Value, dt.Rows[2]["value"]);
         }
     }
 }
