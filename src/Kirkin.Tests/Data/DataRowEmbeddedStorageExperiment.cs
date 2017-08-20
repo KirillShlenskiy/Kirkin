685973584651fcfb6afc98c9a675889feb7d3caf
Kirill <kirill.shlenskiy@gmail.com>
2017-08-20 18:29:43 +1000

    DataRowEmbeddedStorageExperiment: more tests

--- src/Kirkin.Tests/Data/DataRowEmbeddedStorageExperiment.cs
+++ src/Kirkin.Tests/Data/DataRowEmbeddedStorageExperiment.cs
@@ -1,4 +1,8 @@
-﻿using Kirkin.Memory;
+﻿using System;
+using System.Data;
+
+using Kirkin.Data;
+using Kirkin.Memory;
 
 using NUnit.Framework;
 
@@ -7,28 +11,68 @@ namespace Kirkin.Tests.Data
     public class DataRowEmbeddedStorageExperiment
     {
         [Test]
-        public unsafe void DataRowStorageTest()
+        public unsafe void DataRowEmbeddedStorageTest()
         {
-            DataColumn idCol = new IntColumn(0);
-            DataRow row = new DataRow(4);
+            EmbeddedDataColumn idCol = new EmbeddedIntColumn(0);
+            EmbeddedDataColumn valueCol = new EmbeddedIntColumn(4);
+            EnbeddedDataRow row = new EnbeddedDataRow(8);
 
-            for (int i = 0; i < 1000000; i++) {
+            for (int i = 0; i < 1000000; i++)
+            {
                 row[idCol] = 123;
+                row[valueCol] = 321;
             }
 
             Assert.AreEqual(123, row[idCol]);
+            Assert.AreEqual(321, row[valueCol]);
         }
 
-        unsafe sealed class DataRow
+        [Test]
+        public void DataRowStandardTest()
+        {
+            DataTable dt = new DataTable();
+            DataColumn idCol = dt.Columns.Add("ID", typeof(int));
+            DataColumn valueCol = dt.Columns.Add("Value", typeof(int));
+            DataRow row = dt.NewRow();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                row[idCol] = 123;
+                row[valueCol] = 321;
+            }
+
+            Assert.AreEqual(123, row[idCol]);
+            Assert.AreEqual(321, row[valueCol]);
+        }
+
+        [Test]
+        public void DataRowLiteTest()
+        {
+            DataTableLite dt = new DataTableLite();
+            DataColumnLite idCol = dt.Columns.Add("ID", typeof(int));
+            DataColumnLite valueCol = dt.Columns.Add("Value", typeof(int));
+            DataRowLite row = dt.Rows.AddNewRow();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                row[idCol] = 123;
+                row[valueCol] = 321;
+            }
+
+            Assert.AreEqual(123, row[idCol]);
+            Assert.AreEqual(321, row[valueCol]);
+        }
+
+        unsafe sealed class EnbeddedDataRow
         {
             private readonly byte[] bytes;
 
-            public DataRow(int sizeBytes)
+            public EnbeddedDataRow(int sizeBytes)
             {
                 bytes = new byte[sizeBytes];
             }
 
-            public object this[DataColumn column]
+            public object this[EmbeddedDataColumn column]
             {
                 get
                 {
@@ -45,29 +89,31 @@ namespace Kirkin.Tests.Data
             }
         }
 
-        abstract unsafe class DataColumn
+        abstract unsafe class EmbeddedDataColumn
         {
             protected internal abstract object GetValue(void* row);
             protected internal abstract void SetValue(void* row, object value);
         }
 
-        sealed unsafe class IntColumn : DataColumn
+        sealed unsafe class EmbeddedIntColumn : EmbeddedDataColumn
         {
-            public readonly int _offset;
+            public readonly int offset_i32;
 
-            public IntColumn(int offset)
+            public EmbeddedIntColumn(int offset)
             {
-                _offset = offset;
+                if (offset % 4 != 0) throw new ArgumentException("Expecting offset to be divisible by 4.");
+
+                offset_i32 = offset / 4;
             }
 
             protected internal override object GetValue(void* row)
             {
-                return *((int*)row + _offset);
+                return *((int*)row + offset_i32);
             }
 
             protected internal override unsafe void SetValue(void* row, object value)
             {
-                *((int*)row + _offset) = (int)value;
+                *((int*)row + offset_i32) = (int)value;
             }
         }
     }
