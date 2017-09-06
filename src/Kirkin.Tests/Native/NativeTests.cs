4de02f195de34721f47727bab3e33a681a6d7ff6
Kirill <kirill.shlenskiy@gmail.com>
2017-09-06 23:54:18 +1000

    NativeTests: skip tests if Kirkin.Native.dll does not exist

--- src/Kirkin.Tests/Native/NativeTests.cs
+++ src/Kirkin.Tests/Native/NativeTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 using NUnit.Framework;
 
@@ -6,19 +8,34 @@ namespace Kirkin.Tests.Native
 {
     public class NativeTests
     {
+#if DEBUG
+        const string DLL_PATH = @"..\..\..\Debug\Kirkin.Native.dll";
+#else
+        const string DLL_PATH = @"..\..\..\Release\Kirkin.Native.dll";
+#endif
+
         [Test]
         public void NativeAdd()
         {
+            SkipTestIfNativeDllDoesntExist();
+
             Assert.AreEqual(3, NativeMethods.Add(1, 2));
+        }
+
+        private static void SkipTestIfNativeDllDoesntExist()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+            string root = Path.GetDirectoryName(assembly.Location);
+            string dllPath = Path.Combine(root, DLL_PATH);
+
+            if (!File.Exists(dllPath)) {
+                throw new IgnoreException("Kirkin.Native.dll does not exist.");
+            }
         }
 
         static class NativeMethods
         {
-#if DEBUG
-            [DllImport(@"..\..\..\Debug\Kirkin.Native.dll")]
-#else
-            [DllImport(@"..\..\..\Release\Kirkin.Native.dll")]
-#endif
+            [DllImport(DLL_PATH)]
             public static extern int Add(int a, int b);
         }
     }
