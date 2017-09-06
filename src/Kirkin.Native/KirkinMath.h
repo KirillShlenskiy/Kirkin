760cee085c5e50518122ce348c406d14f594e463
Kirill <kirill.shlenskiy@gmail.com>
2017-09-06 21:48:06 +1000

    Kirkin.Native tidy-up

--- src/Kirkin.Native/KirkinMath.h
+++ src/Kirkin.Native/KirkinMath.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#include <stdio.h>
-#include <tchar.h>
-#include <crtdbg.h>
-
- extern "C" __declspec(dllexport) int Add(int a, int b);
+extern "C" __declspec(dllexport) int Add(int a, int b);
