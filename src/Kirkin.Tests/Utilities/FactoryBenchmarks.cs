ce3efe66b8688c9bd33423cfee97db6906314ecd
Kirill <kirill.shlenskiy@gmail.com>
2017-10-12 01:40:27 +1100

    Tests: FactoryBenchmarks added

---
+++ src/Kirkin.Tests/Utilities/FactoryBenchmarks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+
+using NUnit.Framework;
+
+namespace Kirkin.Tests.Utilities
+{
+    public class FactoryBenchmarks
+    {
+        public class DummyClass
+        {
+        }
+
+        public struct DummyStruct
+        {
+        }
+
+        [Test]
+        public void ExpressionFactory()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Func<DummyClass> factory = CreateFactoryViaExpression<DummyClass>();
+                DummyClass instance = factory.Invoke();
+
+                Assert.NotNull(instance);
+            }
+        }
+
+        private static Func<T> CreateFactoryViaExpression<T>()
+        {
+            NewExpression newExpr = Expression.New(typeof(T));
+
+            return Expression
+                .Lambda<Func<T>>(newExpr)
+                .Compile();
+        }
+
+        [Test]
+        public void DynamicMethodFactory()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Func<DummyClass> factory = CreateFactoryViaDynamicMethod<DummyClass>();
+                DummyClass instance = factory.Invoke();
+
+                Assert.NotNull(instance);
+            }
+        }
+
+        private static Func<T> CreateFactoryViaDynamicMethod<T>()
+        {
+            DynamicMethod method = new DynamicMethod("CreateInstance", typeof(T), null);
+            ILGenerator ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Newobj, typeof(T).GetConstructor(Type.EmptyTypes));
+            ilGenerator.Emit(OpCodes.Ret);
+
+            return (Func<T>)method.CreateDelegate(typeof(Func<T>));
+        }
+    }
+}
