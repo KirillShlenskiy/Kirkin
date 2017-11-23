9c9b7cfc01a52739baae137c40df902e99ababd8
Kirill <kirill.shlenskiy@gmail.com>
2017-11-24 09:31:58 +1100

    AssemblyInfo tweaks for NET Standard - work in progress

--- src/Kirkin/Properties/AssemblyInfo.cs
+++ src/Kirkin/Properties/AssemblyInfo.cs
@@ -1,35 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kirkin")]
 [assembly: AssemblyDescription("Core Kirkin component library")]
-[assembly: AssemblyCompany("Kirill Shlenskiy")]
-[assembly: AssemblyProduct("Kirkin")]
-[assembly: AssemblyCopyright("Copyright © Kirill Shlenskiy 2016")]
+[assembly: AssemblyCopyright("Copyright © Kirill Shlenskiy 2017")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95dc82d1-b7c8-4f10-a4a0-747a63382eb6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+#if !NETSTANDARD2_0
+[assembly: AssemblyTitle("Kirkin")]
+[assembly: AssemblyProduct("Kirkin")]
+[assembly: AssemblyCompany("Kirill Shlenskiy")]
 [assembly: AssemblyVersion("1.0.1")]
+#endif
 
 // Allow unit testing internal members.
 [assembly: InternalsVisibleTo("Kirkin.Experimental")]
