f0ee83f04babb50f6bd27b57031ea015bcdeeb5c
Kirill <kirill.shlenskiy@gmail.com>
2017-11-25 10:42:37 +1100

    LambdaTests: nested MemberExpression value resolution

--- src/Kirkin.Tests/LambdaTests.cs
+++ src/Kirkin.Tests/LambdaTests.cs
@@ -15,15 +15,29 @@ namespace Kirkin.Tests
             int y = 2;
             string z = "3";
 
-            Func<int> func = () => x + y + int.Parse(z);
+            DodgyContainer container = new DodgyContainer {
+                Containee = new DodgyContainee {
+                    Value = 4
+                }
+            };
 
-            Assert.AreEqual(6, func());
+            Func<int> func = () => x + y + int.Parse(z) + container.Containee.Value;
+
+            Assert.AreEqual(10, func());
 
             x = 0;
             y = 0;
             z = "0";
 
+            Assert.AreEqual(4, func());
+
+            container.Containee.Value = 0;
+
             Assert.AreEqual(0, func());
+
+            container.Containee = null;
+
+            Assert.Throws<NullReferenceException>(() => func());
         }
 
         [Test]
@@ -33,20 +47,44 @@ namespace Kirkin.Tests
             int y = 2;
             string z = "3";
 
-            Func<int> func = Lambda.CopyAllCaptures(() => x + y + int.Parse(z));
+            DodgyContainer container = new DodgyContainer {
+                Containee = new DodgyContainee {
+                    Value = 4
+                }
+            };
 
-            Assert.AreEqual(6, func());
+            Func<int> func = Lambda.ResolveAllCapturesViaCopy(() => x + y + int.Parse(z) + container.Containee.Value);
+
+            Assert.AreEqual(10, func());
 
             x = 0;
             y = 0;
             z = "0";
 
-            Assert.AreEqual(6, func());
+            Assert.AreEqual(10, func());
+
+            container.Containee.Value = 0;
+
+            Assert.AreEqual(10, func());
+
+            container.Containee = null;
+
+            Assert.AreEqual(10, func());
+        }
+
+        class DodgyContainer
+        {
+            public DodgyContainee Containee;
+        }
+
+        class DodgyContainee
+        {
+            public int Value;
         }
 
         static class Lambda
         {
-            public static Func<T> CopyAllCaptures<T>(Expression<Func<T>> expr)
+            public static Func<T> ResolveAllCapturesViaCopy<T>(Expression<Func<T>> expr)
             {
                 Expression newBody = ConstantResolutionVisitor.Instance.Visit(expr.Body);
 
@@ -65,15 +103,33 @@ namespace Kirkin.Tests
 
                 protected override Expression VisitMember(MemberExpression node)
                 {
-                    ConstantExpression constExpr = (ConstantExpression)node.Expression;
-                    object obj = constExpr.Value;
+                    ConstantExpression constExpr = node.Expression as ConstantExpression;
 
-                    if (node.Member is PropertyInfo prop) {
-                        return Expression.Constant(prop.GetValue(obj));
+                    if (node.Expression is MemberExpression memberExpr)
+                    {
+                        Expression memberValueExpr = VisitMember(memberExpr); // Reduce.
+
+                        if (memberValueExpr is ConstantExpression newConstExpr)
+                        {
+                            constExpr = newConstExpr;
+                        }
+                        else
+                        {
+                            return memberValueExpr;
+                        }
                     }
 
-                    if (node.Member is FieldInfo field) {
-                        return Expression.Constant(field.GetValue(obj));
+                    if (constExpr != null)
+                    {
+                        object obj = constExpr.Value;
+
+                        if (node.Member is PropertyInfo prop) {
+                            return Expression.Constant(prop.GetValue(obj));
+                        }
+
+                        if (node.Member is FieldInfo field) {
+                            return Expression.Constant(field.GetValue(obj));
+                        }
                     }
 
                     return base.VisitMember(node);
