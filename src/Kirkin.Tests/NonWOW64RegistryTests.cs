d720b5cb09322e2975e4170de26c764543a54d24
Kirill <kirill.shlenskiy@gmail.com>
2018-06-17 23:20:14 +1000

    NonWOW64RegistryTests: early work

---
+++ src/Kirkin.Tests/NonWOW64RegistryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.Win32;
+
+using NUnit.Framework;
+
+namespace Kirkin.Tests
+{
+    public class NonWOW64RegistryTests
+    {
+        [Test]
+        public void NonWow64Write()
+        {
+            if (Environment.Is64BitProcess) {
+                Assert.Ignore("Suppressed on x64.");
+            }
+
+            string keyName = Guid.NewGuid().ToString().Replace("-", "");
+            RegistryKey hkcu = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+
+            Assert.Null(hkcu.OpenSubKey($@"Software\Kirkin\{keyName}"));
+
+            RegistryKey key = hkcu.CreateSubKey($@"Software\Kirkin\{keyName}");
+
+            key.SetValue("Test", "Test");
+
+            try
+            {
+                Assert.AreEqual("Test", (string)NonWOW64Registry.GetValue($@"HKEY_CURRENT_USER\Software\Kirkin\{keyName}", "Test", ""));
+                Assert.True(string.IsNullOrEmpty((string)Registry.GetValue($@"HKEY_CURRENT_USER\Software\Kirkin\{keyName}", "Test", "")));
+            }
+            finally
+            {
+                hkcu.DeleteSubKeyTree($@"Software\Kirkin\{keyName}");
+            }
+        }
+    }
+}
