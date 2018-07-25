86f101a6b16ef8b889c8ccfdadc73c6045e5ba60
Kirill <kirill.shlenskiy@gmail.com>
2018-07-25 23:31:34 +1000

    Version change (v1.6.0)

--- src/Kirkin/Properties/AssemblyInfo.cs
+++ src/Kirkin/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Kirkin")]
 [assembly: AssemblyProduct("Kirkin")]
 [assembly: AssemblyCompany("Kirill Shlenskiy")]
-[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
 #endif
 
 // Allow unit testing internal members.
