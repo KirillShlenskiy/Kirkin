6e348fb7360720d4d2b7945b7eee26d5093fba0b
Kirill <kirill.shlenskiy@gmail.com>
2019-01-05 01:36:04 +1100

    Aes256CbcAlgorithm perf

--- src/Kirkin.Experimental/Security/Cryptography/Aes256CbcAlgorithm.cs
+++ src/Kirkin.Experimental/Security/Cryptography/Aes256CbcAlgorithm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace Kirkin.Security.Cryptography
 {
@@ -13,6 +14,21 @@ namespace Kirkin.Security.Cryptography
         /// Generates a random 256-bit key which can be used by an <see cref="Aes256CbcAlgorithm"/> instance.
         /// </summary>
         public static byte[] GenerateKey() => CryptoRandom.GetRandomBytes(32);
+
+        private static readonly ThreadLocal<Aes> s_aes256_cbc_pkcs7 = new ThreadLocal<Aes>(() =>
+        {
+            Aes aes = CryptoFactories.AesFactory();
+
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            return aes;
+        });
+
+        /// <summary>
+        /// Shared thread local AES256 CBC PKCS7 instance.
+        /// </summary>
+        internal static Aes AES256_CBC_PKCS7 => s_aes256_cbc_pkcs7.Value;
 
         /// <summary>
         /// 256 bits/32 bytes (AES 256).
@@ -40,37 +56,28 @@ namespace Kirkin.Security.Cryptography
 
             Array.Copy(iv, 0, output, 0, iv.Length);
 
-            Aes aes = BorrowAes();
-
-            try
+            using (ICryptoTransform transform = AES256_CBC_PKCS7.CreateEncryptor(key, iv))
             {
-                using (ICryptoTransform transform = aes.CreateEncryptor(key, iv))
-                {
-                    if (!transform.CanTransformMultipleBlocks) {
-                        throw new NotSupportedException("AES encryptor does not support multi-block transforms.");
-                    }
-
-                    int blockCount = plaintextBytes.Length / blockSizeInBytes + 1;
-                    int outputOffset = iv.Length;
-
-                    if (blockCount > 1)
-                    {
-                        int count = (blockCount - 1) * blockSizeInBytes;
-
-                        outputOffset += transform.TransformBlock(plaintextBytes, 0, count, output, outputOffset);
-                    }
-
-                    int finalBlockIndex = (blockCount - 1) * blockSizeInBytes;
-                    byte[] finalBlock = transform.TransformFinalBlock(plaintextBytes, finalBlockIndex, plaintextBytes.Length - finalBlockIndex);
-
-                    Array.Copy(finalBlock, 0, output, outputOffset, finalBlock.Length);
-
-                    return outputOffset + finalBlock.Length;
+                if (!transform.CanTransformMultipleBlocks) {
+                    throw new NotSupportedException("AES encryptor does not support multi-block transforms.");
                 }
-            }
-            finally
-            {
-                ReturnAes(aes);
+
+                int blockCount = plaintextBytes.Length / blockSizeInBytes + 1;
+                int outputOffset = iv.Length;
+
+                if (blockCount > 1)
+                {
+                    int count = (blockCount - 1) * blockSizeInBytes;
+
+                    outputOffset += transform.TransformBlock(plaintextBytes, 0, count, output, outputOffset);
+                }
+
+                int finalBlockIndex = (blockCount - 1) * blockSizeInBytes;
+                byte[] finalBlock = transform.TransformFinalBlock(plaintextBytes, finalBlockIndex, plaintextBytes.Length - finalBlockIndex);
+
+                Array.Copy(finalBlock, 0, output, outputOffset, finalBlock.Length);
+
+                return outputOffset + finalBlock.Length;
             }
         }
 
@@ -85,36 +92,27 @@ namespace Kirkin.Security.Cryptography
 
             Array.Copy(ciphertextBytes, 0, iv, 0, iv.Length);
 
-            Aes aes = BorrowAes();
-
-            try
+            using (ICryptoTransform transform = AES256_CBC_PKCS7.CreateDecryptor(key, iv))
             {
-                using (ICryptoTransform transform = aes.CreateDecryptor(key, iv))
-                {
-                    if (!transform.CanTransformMultipleBlocks) {
-                        throw new NotSupportedException("AES encryptor does not support multi-block transforms.");
-                    }
-
-                    int blockCount = (ciphertextBytes.Length - iv.Length) / blockSizeInBytes;
-                    int outputOffset = 0;
-
-                    if (blockCount > 1)
-                    {
-                        int count = (blockCount - 1) * blockSizeInBytes;
-
-                        outputOffset = transform.TransformBlock(ciphertextBytes, iv.Length, count, output, 0);
-                    }
-
-                    byte[] finalBlock = transform.TransformFinalBlock(ciphertextBytes, ciphertextBytes.Length - blockSizeInBytes, blockSizeInBytes);
-
-                    Array.Copy(finalBlock, 0, output, outputOffset, finalBlock.Length);
-
-                    return outputOffset + finalBlock.Length;
+                if (!transform.CanTransformMultipleBlocks) {
+                    throw new NotSupportedException("AES encryptor does not support multi-block transforms.");
                 }
-            }
-            finally
-            {
-                ReturnAes(aes);
+
+                int blockCount = (ciphertextBytes.Length - iv.Length) / blockSizeInBytes;
+                int outputOffset = 0;
+
+                if (blockCount > 1)
+                {
+                    int count = (blockCount - 1) * blockSizeInBytes;
+
+                    outputOffset = transform.TransformBlock(ciphertextBytes, iv.Length, count, output, 0);
+                }
+
+                byte[] finalBlock = transform.TransformFinalBlock(ciphertextBytes, ciphertextBytes.Length - blockSizeInBytes, blockSizeInBytes);
+
+                Array.Copy(finalBlock, 0, output, outputOffset, finalBlock.Length);
+
+                return outputOffset + finalBlock.Length;
             }
         }
 
@@ -132,24 +130,6 @@ namespace Kirkin.Security.Cryptography
             int ivLength = BlockSize / 8;
 
             return ciphertextBytes.Length - ivLength;
-        }
-
-        private static Aes BorrowAes()
-        {
-            Aes aes = CryptoFactories.AesFactory();
-
-            if (aes.Mode != CipherMode.CBC || aes.Padding != PaddingMode.PKCS7)
-            {
-                aes.Mode = CipherMode.CBC;
-                aes.Padding = PaddingMode.PKCS7;
-            }
-
-            return aes;
-        }
-
-        private static void ReturnAes(Aes aes)
-        {
-            aes.Dispose();
         }
     }
 }
