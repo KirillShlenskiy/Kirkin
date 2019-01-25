cda888a74ec3e9d53c74e37f70320f1cb2e1cb06
Kirill <kirill.shlenskiy@gmail.com>
2019-01-26 00:25:09 +1100

    Aes256GcmTests tweaks

--- src/Kirkin.Tests/Security/Cryptography/Aes256GcmTests.cs
+++ src/Kirkin.Tests/Security/Cryptography/Aes256GcmTests.cs
@@ -17,7 +17,7 @@ namespace Kirkin.Tests.Security.Cryptography
         [Test]
         public void BasicEncryption()
         {
-            byte[] plaintextBytes = CryptoRandom.GetRandomBytes(4096);
+            byte[] plaintextBytes = CryptoRandom.GetRandomBytes(1024 * 8);
             byte[] key = CryptoRandom.GetRandomBytes(32);
             byte[] iv = CryptoRandom.GetRandomBytes(12);
 
@@ -26,38 +26,42 @@ namespace Kirkin.Tests.Security.Cryptography
             AeadParameters aeadParameters = new AeadParameters(keyParameter, MAC_LENGTH_BYTES * 8, iv);
 
             // Encrypt.
-            GcmBlockCipher encryptor = new GcmBlockCipher(new AesEngine());
-
-            encryptor.Init(true, aeadParameters);
-
-            byte[] ciphertextBytes = new byte[encryptor.GetOutputSize(plaintextBytes.Length)];
+            byte[] encryptedBytes;
 
             {
-                int offset = encryptor.ProcessBytes(plaintextBytes, 0, plaintextBytes.Length, ciphertextBytes, 0);
-                int finalLength = offset + encryptor.DoFinal(ciphertextBytes, offset);
+                GcmBlockCipher encryptor = new GcmBlockCipher(new AesEngine());
 
-                if (finalLength != ciphertextBytes.Length) {
-                    Array.Resize(ref ciphertextBytes, finalLength);
+                encryptor.Init(true, aeadParameters);
+
+                encryptedBytes = new byte[encryptor.GetOutputSize(plaintextBytes.Length)];
+
+                int offset = encryptor.ProcessBytes(plaintextBytes, 0, plaintextBytes.Length, encryptedBytes, 0);
+                int finalLength = offset + encryptor.DoFinal(encryptedBytes, offset);
+
+                if (finalLength != encryptedBytes.Length) {
+                    Array.Resize(ref encryptedBytes, finalLength);
                 }
             }
 
             // Decrypt.
-            GcmBlockCipher decryptor = new GcmBlockCipher(new AesEngine());
-
-            decryptor.Init(false, aeadParameters);
-
-            byte[] roundtripBytes = new byte[decryptor.GetOutputSize(ciphertextBytes.Length)];
+            byte[] decryptedBytes;
 
             {
-                int offset = decryptor.ProcessBytes(ciphertextBytes, 0, ciphertextBytes.Length, roundtripBytes, 0);
-                int finalLength = offset + decryptor.DoFinal(roundtripBytes, offset);
+                GcmBlockCipher decryptor = new GcmBlockCipher(new AesEngine());
 
-                if (finalLength != roundtripBytes.Length) {
-                    Array.Resize(ref roundtripBytes, finalLength);
+                decryptor.Init(false, aeadParameters);
+
+                decryptedBytes = new byte[decryptor.GetOutputSize(encryptedBytes.Length)];
+
+                int offset = decryptor.ProcessBytes(encryptedBytes, 0, encryptedBytes.Length, decryptedBytes, 0);
+                int finalLength = offset + decryptor.DoFinal(decryptedBytes, offset);
+
+                if (finalLength != decryptedBytes.Length) {
+                    Array.Resize(ref decryptedBytes, finalLength);
                 }
             }
 
-            Assert.AreEqual(plaintextBytes, roundtripBytes);
+            Assert.AreEqual(plaintextBytes, decryptedBytes);
         }
     }
 }
